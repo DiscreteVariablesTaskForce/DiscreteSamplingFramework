905648523cad9ace7dc03018fe9728f814f2d9a8
Alex Phillips <alecks.phillips@gmail.com>
2021-09-16 17:16:34 +0100

    Add base classes
    Classes for DiscreteVariable and DiscreteVariableDistribution

---
+++ discretesampling/discrete.py
@@ -0,0 +1,15 @@
+class DiscreteVariable:    
+    def __init__(self):
+        pass
+
+class DiscreteVariableDisribution:    
+    def __init__(self):
+        pass
+        
+    def sample(self):
+        #Do something more useful ideally
+        x = DiscreteVariable()
+        return x
+    
+    def eval(self):
+        return 0
