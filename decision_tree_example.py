c741168bfe9156ff9d1cfebe76360a1a95ede531
Alex Phillips <alecks.phillips@gmail.com>
2021-11-29 13:29:43 +0000

    Update decision_tree_example.py

--- decision_tree_example.py
+++ decision_tree_example.py
@@ -5,16 +5,14 @@ Created on Fri Nov  5 14:28:12 2021
 @author: efthi
 """
 
+from discretesampling import decision_tree
+
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from create_tree import Tree
 import copy
-from Tree_sample import TreeDistribution, forward, reverse
 import random
-from Metrics import stats, accuracy
 import math
 import numpy as np
-from Test_tree import test
 
 data = datasets.load_wine()
 
@@ -23,7 +21,7 @@ y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.30,random_state=5)
 
-initialTree = Tree(X_train, y_train)
+initialTree = decision_tree.Tree(X_train, y_train)
 
 a = 0.01
 b = 5
@@ -33,15 +31,14 @@ forward_probs = []
 sampledTrees = []
 
 for i in range (5000):
-
-    forward_proposal = TreeDistribution(currentTree)
+    forward_proposal = decision_tree.TreeDistribution(currentTree)
     sampleTree = forward_proposal.sample()
     forward_probability = forward_proposal.eval(sampleTree)
-    forward_probability = forward(forward_probs, forward_probability)
+    forward_probability = decision_tree.forward(forward_probs, forward_probability)
     
-    reverse_proposal = TreeDistribution(sampleTree)
+    reverse_proposal = decision_tree.TreeDistribution(sampleTree)
     reverse_probability = reverse_proposal.eval(currentTree)
-    reverse_probability = reverse(forward_probs, reverse_probability)
+    reverse_probability = decision_tree.reverse(forward_probs, reverse_probability)
     
     new_tree_target = sampleTree.evaluatePosterior(a,b)
     current_tree_target = currentTree.evaluatePosterior(a,b)
@@ -49,7 +46,6 @@ for i in range (5000):
     targetRatio = new_tree_target - current_tree_target
     proposalRatio = math.log(reverse_probability) - math.log(forward_probability)
     acceptLogProbability = min(1, math.exp(targetRatio + proposalRatio))
-
     
     q= random.random()
     if ((acceptLogProbability) > q):
@@ -57,13 +53,10 @@ for i in range (5000):
     else:
         currentTree = currentTree
         del forward_probs[-1]
-        
+    
     
     sampledTrees.append(copy.deepcopy(currentTree))
 
- 
-
-labels = stats.predict(currentTree, X_test)
-acc = accuracy(y_test, labels)
-
+labels = decision_tree.stats.predict(currentTree, X_test)
+acc = decision_tree.accuracy(y_test, labels)
 
