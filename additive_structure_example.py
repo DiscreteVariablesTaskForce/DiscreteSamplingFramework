2c44d290f694aa11c3413bd2e1d9cdb141e2c62a
Alex Phillips <alecks.phillips@gmail.com>
2021-11-30 12:02:37 +0000

    Refactor additive_structure into OOP

--- additive_structure_example.py
+++ additive_structure_example.py
@@ -1,12 +1,18 @@
 import numpy as np
-from split_merge import *
+import discretesampling.additive_structure as addstruct
 
 # Starting with a discrete set of numbers
-initial_set = np.array([[1, 2], [3], [4, 5], [6], [7, 8, 9]])
+initial_set = [[1, 2], [3], [4, 5], [6], [7, 8, 9]]
+initialAS = addstruct.AdditiveStructure(initial_set)
 
-# We want to either split one subset or merge two
-sp = SplitMergeMove()
-new_set, prob, rev = sp.propose(discrete_set=initial_set)  # the new set, the log probability and reverse
-print("New subset", new_set)
+forward_proposal = addstruct.AdditiveStructureDistribution(initialAS)
+proposedAS = forward_proposal.sample()
 
-log_prob = sp.probability_merge(frac=1, num_set=2)      # log probability of merging two sets
+forward_probability = forward_proposal.eval(proposedAS)
+
+reverse_proposal = addstruct.AdditiveStructureDistribution(proposedAS)
+
+reverse_probability = reverse_proposal.eval(initialAS)
+
+proposalratio = reverse_probability - forward_probability
+
--- discretesampling/additive_structure/split_merge.py
+++ discretesampling/additive_structure/split_merge.py
@@ -1,111 +1,138 @@
-from math import log
+from math import log, inf
 from random import randint, random, choices
 import random
 import numpy as np
-from numbers import binomial, stirling
+import copy
+from .numbers import binomial, stirling
+from .. import discrete
 
-
-class ProposedMoves:
-    def __init__(self):
-        pass
-
-
-class SplitMergeMove(ProposedMoves):
-    def propose(self, discrete_set):
-        """
-        Given an initial set, if both split and merge can be performed,
-        we randomly choose a move and sample a new set based on the initial.
-        """
-        multi_subsets = [subset for subset in discrete_set if len(subset) > 1]
-
-        if len(multi_subsets) == 0:
-            return self.merge_subset(discrete_set, frac=1)
-        elif len(discrete_set) == 1:
-            return self.split_subset(discrete_set, frac=1)
-        elif random() < 0.5:
-            return self.merge_subset(discrete_set, frac=2)
-        else:
-            return self.split_subset(discrete_set, frac=2)
-
-    def split_subset(self, discrete_set, frac):
+class AdditiveStructure(discrete.DiscreteVariable):
+    def __init__(self,discrete_set):
+        self.discrete_set = discrete_set
+        
+    @classmethod
+    def getDistributionType(self):
+        return AdditiveStructureDistribution
+    
+    def split_subset(self, frac):
         """
         :param discrete_set: initial set
         :param frac: 1 or 2 indicating the probability of choosing split
         :return: new set, log probability of moving to new set, reverse log probability
         """
-        single_subsets = [subset for subset in discrete_set if len(subset) is 1]
-        multi_subsets = [subset for subset in discrete_set if len(subset) > 1]
-
+        single_subsets = [subset for subset in self.discrete_set if len(subset) == 1]
+        multi_subsets = [subset for subset in self.discrete_set if len(subset) > 1]
+        
         if len(multi_subsets) == 0:     # there is no subset that can be split
             return single_subsets
-
+        
         # Get a random subset from the list
         index = randint(0, len(multi_subsets)-1)
         subset_to_split = np.array(multi_subsets[index])
-
+        
         # assign each element to a subset
         split_vec = choices([True, False], k=len(subset_to_split))
         while np.sum(split_vec) == 0 or np.sum(np.logical_not(split_vec)) == 0:
             split_vec = choices([True, False], k=len(subset_to_split))
-
+        
         multi_subsets.pop(index)
         multi_subsets.append(list(subset_to_split[split_vec]))
         multi_subsets.append(list(subset_to_split[np.logical_not(split_vec)]))
-
+        
         new_set = multi_subsets + single_subsets
         new_set.sort()
-
-        new_singleton = [subset for subset in discrete_set if len(subset) is 1]
+        
+        new_singleton = [subset for subset in self.discrete_set if len(subset) is 1]
         if len(new_set) == len(new_singleton):
             frac_rev = 1
         else:
             frac_rev = 2
+        
+        return AdditiveStructure(new_set)
 
-        proposal_logprob = self.probability_split(frac, len(multi_subsets), len(subset_to_split))
-        reverse_logprob = self.probability_merge(frac_rev, len(new_set))
-
-        return new_set, proposal_logprob, reverse_logprob
-
-    def merge_subset(self, discrete_set, frac):
+    def merge_subset(self, frac):
         """
         :param discrete_set: initial set
         :param frac: 1 or 2 indicating the probability of choosing merge
         :return: new set, log probability of moving to new set, reverse log probability
         """
-        if len(discrete_set) == 1:
-            return discrete_set
-
-        index_1 = randint(0, len(discrete_set)-1)
-        remaining = list(range(len(discrete_set)))
+        if len(self.discrete_set) == 1:
+            return copy.deepcopy(self)
+        
+        index_1 = randint(0, len(self.discrete_set)-1)
+        remaining = list(range(len(self.discrete_set)))
         remaining.pop(index_1)
-        index_2 = randint(0, len(remaining)-1)
-
-        first_subset = discrete_set[index_1]
-        second_subset = discrete_set[index_2]
-
+        index_2 = random.choice(remaining)
+        
+        first_subset = self.discrete_set[index_1]
+        second_subset = self.discrete_set[index_2]
+        
         new_subset = first_subset + second_subset
         new_subset.sort()
+        
+        new_discrete_set = [i for i in self.discrete_set if (i != first_subset and i != second_subset)]
+        new_discrete_set.append(new_subset)
+        new_discrete_set.sort()
+        
+        return AdditiveStructure(new_discrete_set)
 
-        discrete_set = [i for i in discrete_set if i != first_subset or i != second_subset]
-        discrete_set.append(new_subset)
-        discrete_set.sort()
 
-        multi_subsets = [subset for subset in discrete_set if len(subset) > 1]
-
-        if len(discrete_set) == 1:
-            frac_rev = 1
+class AdditiveStructureDistribution(discrete.DiscreteVariableDistribution):
+    def __init__(self, current: AdditiveStructure):
+        self.current = current
+        self.multi_subsets = [subset for subset in self.current.discrete_set if len(subset) > 1]
+    
+    def eval(self, proposed: AdditiveStructure):
+        #For now assume there is a valid move
+        #TODO: add check that elements in current and proposed are the same
+        current_num_sets = len(self.current.discrete_set)
+        proposed_num_sets = len(proposed.discrete_set)
+        
+        logprob = -inf
+        
+        #Figure out whether we do a split or merge to go from current to proposed
+        frac = 2
+        if (current_num_sets > proposed_num_sets):
+            if len(self.multi_subsets) == 0:
+                frac = 1
+            else:
+                frac = 2
+                
+            logprob = self.probability_merge(frac, len(self.current.discrete_set))
+        elif (proposed_num_sets > current_num_sets):
+            if len(self.current.discrete_set) == 1:
+                frac = 1
+            else:
+                frac = 2
+            #Figure out which subset needs to be split to go from current to proposed
+            #(i.e. the set in current which isn't in proposed)
+            subset_to_split =  [i for i in self.current.discrete_set if proposed.discrete_set.count(i) < 1][0]
+            logprob = self.probability_split(frac, len(self.multi_subsets), len(subset_to_split))
         else:
-            frac_rev = 2
+            #Else proposed_num_sets == current_num_sets which is not allowed
+            logprob = -inf
 
-        proposal_logprob = self.probability_merge(frac, len(discrete_set))
-        reverse_logprob = self.probability_split(frac_rev, len(multi_subsets), len(new_subset))
+        return logprob
 
-        return discrete_set, proposal_logprob, reverse_logprob
-
+    def sample(self):
+        """
+        Given an initial set, if both split and merge can be performed,
+        we randomly choose a move and sample a new set based on the initial.
+        """
+        if len(self.multi_subsets) == 0:
+            return self.current.merge_subset(frac=1)
+        elif len(self.current.discrete_set) == 1:
+            return self.current.split_subset(frac=1)
+        elif random.random() < 0.5:
+            return self.current.merge_subset(frac=2)
+        else:
+            return self.current.split_subset(frac=2)
+    
     @staticmethod
     def probability_merge(frac, num_set):
         return -log(frac) - log(binomial(num_set, 2))
-
+    
     @staticmethod
     def probability_split(frac, num_non_single, elements_subset):
         return -log(frac) - log(num_non_single) - log(stirling(elements_subset, 2))
+        
