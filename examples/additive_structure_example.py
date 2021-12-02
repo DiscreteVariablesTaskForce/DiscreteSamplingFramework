b8a0b9d622fef366ec678ca4ac84983a7c3a520f
Alex Phillips <alecks.phillips@gmail.com>
2021-12-02 16:11:47 +0000

    Update additive_structure_example.py
    Update example to use MCMC and SMC sampler classes

--- examples/additive_structure_example.py
+++ examples/additive_structure_example.py
@@ -1,24 +1,12 @@
 import discretesampling.additive_structure as addstruct
-import math
-
-# Starting with a discrete set of numbers
-initial_set = [[1, 2], [3], [4, 5], [6], [7, 8, 9]]
-initialAS = addstruct.AdditiveStructure(initial_set)
+from discretesampling.algorithms import DiscreteVariableMCMC, DiscreteVariableSMC
 
 data = [] #some data defining the target
 target = addstruct.AdditiveStructureTarget(data)
+initialProposal = addstruct.AdditiveStructureInitialProposal([1,2,3,4,5])
 
-forward_proposal = addstruct.AdditiveStructureProposal(initialAS)
-proposedAS = forward_proposal.sample()
+asMCMC = DiscreteVariableMCMC(addstruct.AdditiveStructure, target, initialProposal)
+asSamples = asMCMC.sample(1000)
 
-forward_logprob = forward_proposal.eval(proposedAS)
-
-reverse_proposal = addstruct.AdditiveStructureProposal(proposedAS)
-
-reverse_logprob = reverse_proposal.eval(initialAS)
-
-current_target_logprob = target.eval(initialAS)
-proposed_target_logprob = target.eval(proposedAS)
-
-log_acceptance_ratio = proposed_target_logprob - current_target_logprob + reverse_logprob - forward_logprob
-acceptance_probability = min(1, math.exp(log_acceptance_ratio))
+asSMC = DiscreteVariableSMC(addstruct.AdditiveStructure, target, initialProposal)
+asSamples = asSMC.sample(N=10,P=1000)
