11f76c4ef9f6c2228c2531a8beda795dc4bf1e0e
Alex Phillips <alecks.phillips@gmail.com>
2021-12-02 11:58:32 +0000

    Add DiscreteVariableInitialProposal

--- discretesampling/discrete.py
+++ discretesampling/discrete.py
@@ -40,6 +40,33 @@ class DiscreteVariableProposal:
         return logp
 
 
+#Exact same as proposal above
+class DiscreteVariableInitialProposal:    
+    def __init__(self, values, probs):
+        #Check dims and probs are valid
+        assert len(values) == len(probs), "Invalid PMF specified, x and p of different lengths"
+        probs = np.array(probs)
+        tolerance = np.sqrt(np.finfo(np.float64).eps)
+        assert abs(1 - sum(probs)) < tolerance, "Invalid PMF specified, sum of probabilities !~= 1.0"
+        
+        self.x = values
+        self.pmf = probs
+        self.cmf = np.cumsum(probs)        
+        
+    def sample(self):
+        q = random.random() #random unif(0,1)
+        return self.x[np.argmax(self.cmf >= q)]
+    
+    def eval(self, y):
+        try:
+            i = self.x.index(y)
+            logp = math.log(self.pmf[i])
+        except ValueError:
+            print("Warning: value " + str(y) + " not in pmf")
+            logp = -math.inf
+        return logp
+
+
 class DiscreteVariableTarget:
     def __init__(self):
         pass
