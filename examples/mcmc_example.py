d2887797d13132791a0bdddc9fa25e73393e0498
Alex Phillips <alecks.phillips@gmail.com>
2021-12-09 17:40:29 +0000

    Fix examples
    mcmc_example and smc_example were not compatible with recent changes

--- examples/mcmc_example.py
+++ examples/mcmc_example.py
@@ -12,15 +12,15 @@ y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.30,random_state=5)
 
-initialTree = dt.Tree(X_train, y_train)
 a = 0.01
 b = 5
 
 target = dt.TreeTarget(a,b)
+initialProposal = dt.TreeInitialProposal(X_train, y_train)
 
 #Create an MCMC sampler on type dt.Tree with target distribution target
-dtMCMC = DiscreteVariableMCMC(dt.Tree, target)
+dtMCMC = DiscreteVariableMCMC(dt.Tree, target, initialProposal)
 
-treeSamples = dtMCMC.sample(1000, initialTree)
+treeSamples = dtMCMC.sample(N = 1000)
 mcmc_acc = [dt.accuracy(y_test, dt.stats(x,X_test).predict(X_test)) for x in treeSamples]
-print(numpy.mean(mcmc_acc[501:1000]))
+print(numpy.mean(mcmc_acc[501:999]))
--- examples/smc_example.py
+++ examples/smc_example.py
@@ -12,15 +12,15 @@ y = data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.30,random_state=5)
 
-initialTree = dt.Tree(X_train, y_train)
 a = 0.01
 b = 5
 
 target = dt.TreeTarget(a,b)
+initialProposal = dt.TreeInitialProposal(X_train, y_train)
 
-dtSMC = DiscreteVariableSMC(dt.Tree, target)
+dtSMC = DiscreteVariableSMC(dt.Tree, target, initialProposal)
 
-treeSamples = dtSMC.sample(10,1000, initialTree)
+treeSamples = dtSMC.sample(N = 10, P = 1000)
 
 smc_acc = [dt.accuracy(y_test, dt.stats(x,X_test).predict(X_test)) for x in treeSamples]
 print(numpy.mean(smc_acc))
