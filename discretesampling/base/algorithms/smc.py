fdebf45ec6e1a0bc1e10a827c9c54de2b60e09b4
EfthyvoulosDrousiotis <E.Drousiotis@liverpool.ac.uk>
2022-04-13 10:53:26 +0100

    updated smc.py

--- discretesampling/base/algorithms/smc.py
+++ discretesampling/base/algorithms/smc.py
@@ -43,6 +43,9 @@ class DiscreteVariableSMC():
                       for p in initialParticles]
         logWeights = normaliseLogWeights(logWeights)
         for i in range(N):
+            current_particles_for_l_kernel = []
+            new_particles_for_l_kernel = []
+            forward_logprob_idx = []
             logNeff = calculateNeff(logWeights)
             print("Neff = ", math.exp(logNeff))
             if (logNeff < math.log(P) - math.log(2)):
@@ -60,40 +63,73 @@ class DiscreteVariableSMC():
             new_logWeights = copy.deepcopy(logWeights)
 
             forward_logprob = np.zeros(len(current_particles))
+            
 
             # Sample new particles and calculate forward probabilities
             for p in range(P):
                 forward_proposal = self.proposalType(current_particles[p])
                 new_particles[p] = forward_proposal.sample()
                 forward_logprob[p] = forward_proposal.eval(new_particles[p])
+                
+            #####new code
+            if type(np.where(forward_logprob == np.min(forward_logprob))) == int: 
+                current_particles_for_l_kernel.append(current_particles[np.where(forward_logprob == np.min(forward_logprob))])
+                new_particles_for_l_kernel.append(new_particles[np.where(forward_logprob == np.min(forward_logprob))])
+                forward_logprob_idx.append(forward_logprob[np.where(forward_logprob == np.min(forward_logprob))])
+            else:
+                current_particles_for_l_kernel.append(current_particles[np.where(forward_logprob == np.min(forward_logprob))[0][0]])
+                new_particles_for_l_kernel.append(new_particles[np.where(forward_logprob == np.min(forward_logprob))[0][0]])
+                forward_logprob_idx.append(forward_logprob[np.where(forward_logprob == np.min(forward_logprob))[0][0]])
+                
+            median = np.median(forward_logprob)
+            idx = (np.abs(forward_logprob - median)).argmin()
+            current_particles_for_l_kernel.append(current_particles[idx])
+            new_particles_for_l_kernel.append(new_particles[idx])
+            forward_logprob_idx.append(forward_logprob[idx])
+            
+            if type(np.where(forward_logprob == np.max(forward_logprob))) == int: 
+                current_particles_for_l_kernel.append(current_particles[np.where(forward_logprob == np.max(forward_logprob))])
+                new_particles_for_l_kernel.append(new_particles[np.where(forward_logprob == np.max(forward_logprob))])
+                forward_logprob_idx.append(forward_logprob[np.where(forward_logprob == np.max(forward_logprob))])
+            else:
+                current_particles_for_l_kernel.append(current_particles[np.where(forward_logprob == np.max(forward_logprob))[0][0]])
+                new_particles_for_l_kernel.append(new_particles[np.where(forward_logprob == np.min(forward_logprob))[0][0]])
+                forward_logprob_idx.append(forward_logprob[np.where(forward_logprob == np.max(forward_logprob))[0][0]])
+            ###new code                      
+            
+            
+                                  
 
+            
             new_logWeights = np.full([P], -math.inf)
             if self.use_optimal_L:
-                Lkernel = self.LKernelType(
-                    new_particles, current_particles, parallel=self.parallel,
+                Lkernel = self.LKernelType(#changed the arguments when we call this functions
+                    new_particles_for_l_kernel, current_particles_for_l_kernel, parallel=self.parallel,
                     num_cores=self.num_cores
                 )
-            for p in range(P):
+            for p in range(len(new_particles_for_l_kernel)):#changed that from reange(P) to len(new_particles_for_l_kernel)
                 if self.use_optimal_L:
                     reverse_logprob = Lkernel.eval(p)
-
+                
                 else:
-                    Lkernel = self.LKernelType(new_particles[p])
-                    reverse_logprob = Lkernel.eval(current_particles[p])
-
+                    Lkernel = self.LKernelType(new_particles_for_l_kernel[p])#changed the arguments when we call this functions
+                    reverse_logprob = Lkernel.eval(current_particles_for_l_kernel[p])#changed the arguments when we call this functions
+            
+                
+            for p in range(P):
                 current_target_logprob = self.target.eval(current_particles[p])
                 new_target_logprob = self.target.eval(new_particles[p])
-
+                idx = (np.abs(forward_logprob_idx - forward_logprob[p])).argmin()
                 new_logWeights[p] = (new_target_logprob
                                      - current_target_logprob
                                      + reverse_logprob
-                                     - forward_logprob[p]
+                                     - forward_logprob[idx]
                                      + logWeights[p])
 
             logWeights = normaliseLogWeights(new_logWeights)
-
+            
             current_particles = new_particles
-
+        
         return current_particles
 
 
