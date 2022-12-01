98f3467c10928117dd3bd56c7e9b0da4f105279b
Alex Phillips <alecks.phillips@gmail.com>
2022-12-01 17:56:02 +0000

    chore: add example using MPI

---
+++ examples/smc_MPI_example.py
@@ -0,0 +1,35 @@
+import numpy
+from discretesampling.base.algorithms import DiscreteVariableSMC
+from discretesampling.base.executor import Executor_MPI
+import discretesampling.domain.decision_tree as dt
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+data = datasets.load_wine()
+
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,
+                                                    random_state=5)
+
+a = 0.01
+b = 5
+
+target = dt.TreeTarget(a, b)
+initialProposal = dt.TreeInitialProposal(X_train, y_train)
+
+# Necessary to use mpi4py
+if __name__ == "__main__":
+    dtSMC = DiscreteVariableSMC(dt.Tree, target, initialProposal,
+                                use_optimal_L=False, executor=Executor_MPI())
+
+    try:
+        treeSamples = dtSMC.sample(N=10, P=100)
+
+        smc_acc = [dt.accuracy(y_test, dt.stats(x, X_test).predict(X_test))
+                   for x in treeSamples]
+        print(numpy.mean(smc_acc))
+    except ZeroDivisionError:
+        print("SMC sampling failed due to division by zero")
