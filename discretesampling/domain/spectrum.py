4bb268523e0e5d488c8d86f22e05c1e6e2496bab
Alex Phillips <alecks.phillips@gmail.com>
2023-05-03 18:26:56 +0100

    feat: add evaluatePrior to SpectrumTarget

--- discretesampling/domain/spectrum.py
+++ discretesampling/domain/spectrum.py
@@ -70,5 +70,9 @@ class SpectrumDimensionTarget(types.DiscreteVariableTarget):
 
     def eval(self, x):
         # Evaluate logposterior at point x, P(x|D) \propto P(D|x)P(x)
-        target = nbinom(self.n, self.p).logpmf(x.value)
+        target = self.evaluatePrior(x)
         return target
+    
+    def evaluatePrior(self, x):
+        logprob = nbinom(self.n, self.p).logpmf(x.value)
+        return logprob
