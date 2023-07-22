61ce48e01b30983c9fc2ad90b83a8e94694ea022
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-07-22 08:34:28 +0100

    chore: separate MPI examples CI fails on MPI examples, test separately

--- examples/decision_tree_example.py
+++ examples/decision_tree_example.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov  5 14:28:12 2021
+
 @author: efthi
 """
-from discretesampling.base.util import gather_all
-from mpi4py import MPI
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
-from discretesampling.base.algorithms import DiscreteVariableSMC
-from discretesampling.domain import decision_tree as dt
-import sys
-sys.path.append('../')  # Looks like mpiexec won't find discretesampling package without appending '../'
 
+from discretesampling.domain import decision_tree as dt
+from discretesampling.base.algorithms import DiscreteVariableMCMC, DiscreteVariableSMC
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+import numpy as np
 
 data = datasets.load_wine()
 
@@ -26,36 +25,25 @@ b = 5
 target = dt.TreeTarget(a, b)
 initialProposal = dt.TreeInitialProposal(X_train, y_train)
 
-N = 1 << 10
-T = 10
-num_MC_runs = 1
+dtMCMC = DiscreteVariableMCMC(dt.Tree, target, initialProposal)
+try:
+    treeSamples = dtMCMC.sample(500)
+
+    mcmcLabels = [dt.stats(x, X_test).predict(X_test) for x in treeSamples]
+    mcmcAccuracy = [dt.accuracy(y_test, x) for x in mcmcLabels]
+
+    print("MCMC mean accuracy: ", np.mean(mcmcAccuracy[250:499]))
+except ZeroDivisionError:
+    print("MCMC sampling failed due to division by zero")
+
 
 dtSMC = DiscreteVariableSMC(dt.Tree, target, initialProposal)
 try:
-    runtimes = []
-    accuracies = []
-    for seed in range(num_MC_runs):
-        MPI.COMM_WORLD.Barrier()
-        start = MPI.Wtime()
-        # seed = np.random.randint(0, 32766)
-        if MPI.COMM_WORLD.Get_rank() == 0:
-            print("seed = ", seed)
-        treeSMCSamples = dtSMC.sample(T, N, seed)
-        MPI.COMM_WORLD.Barrier()
-        end = MPI.Wtime()
-        if MPI.COMM_WORLD.Get_size() > 1:
-            treeSMCSamples = gather_all(treeSMCSamples)
+    treeSMCSamples = dtSMC.sample(10, 1000)
 
-        smcLabels = [dt.stats(x, X_test).predict(X_test) for x in treeSMCSamples]
-        smcAccuracy = [dt.accuracy(y_test, x) for x in smcLabels]
+    smcLabels = [dt.stats(x, X_test).predict(X_test) for x in treeSMCSamples]
+    smcAccuracy = [dt.accuracy(y_test, x) for x in smcLabels]
 
-        if MPI.COMM_WORLD.Get_rank() == 0:
-            accuracies.append(np.mean(smcAccuracy))  # replace mean with majority voting
-            runtimes.append(end-start)
-            print("SMC mean accuracy: ", accuracies[-1])
-            print("SMC run-time: ", runtimes[-1])
-    if MPI.COMM_WORLD.Get_rank() == 0:
-        print("SMC mean of mean accuracies: ", np.mean(accuracies))
-        print("SMC median runtime: ", np.median(runtimes))
+    print("SMC mean accuracy: ", np.mean(smcAccuracy))
 except ZeroDivisionError:
     print("SMC sampling failed due to division by zero")
