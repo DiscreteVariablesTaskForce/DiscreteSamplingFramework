db2663262493843000ad25d3a133dcf6c411895d
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-07-25 12:21:53 +0100

    fix: base initial proposal

--- discretesampling/base/types.py
+++ discretesampling/base/types.py
@@ -82,9 +82,33 @@ class DiscreteVariableProposal:
 
 
 # Exact same as proposal above
-class DiscreteVariableInitialProposal(DiscreteVariableProposal):
-    def sample(self, target=None):
-        return super().sample()
+class DiscreteVariableInitialProposal():
+    def __init__(self, values, probs):
+        # Check dims and probs are valid
+        assert len(values) == len(probs), "Invalid PMF specified, x and p" +\
+            " of different lengths"
+        probs = np.array(probs)
+        tolerance = np.sqrt(np.finfo(np.float64).eps)
+        assert abs(1 - sum(probs)) < tolerance, "Invalid PMF specified," +\
+            " sum of probabilities !~= 1.0"
+        assert all(probs > 0), "Invalid PMF specified, all probabilities" +\
+            " must be > 0"
+        self.x = values
+        self.pmf = probs
+        self.cmf = np.cumsum(probs)
+
+    def sample(self, rng=RNG(), target=None):
+        q = rng.random()  # random unif(0,1)
+        return self.x[np.argmax(self.cmf >= q)]
+
+    def eval(self, y, target=None):
+        try:
+            i = self.x.index(y)
+            logp = math.log(self.pmf[i])
+        except ValueError:
+            print("Warning: value " + str(y) + " not in pmf")
+            logp = -math.inf
+        return logp
 
 
 class DiscreteVariableTarget:
