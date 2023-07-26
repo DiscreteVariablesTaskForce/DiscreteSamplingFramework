06e27edb825cc59ebd164691c9e5ab4d454f6201
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-07-26 20:50:24 +0100

    tests: add tests for additive structure proposal
    Add tests for addititve structure proposal to tests/test_additive_structure.py

--- tests/test_additive_structure.py
+++ tests/test_additive_structure.py
@@ -1,5 +1,63 @@
 import pytest
-from discretesampling.domain.additive_structure.additive_structure import AdditiveStructure
+import numpy as np
+from discretesampling.base.random import RNG
+from discretesampling.domain.additive_structure import (
+    AdditiveStructure, AdditiveStructureProposal, AdditiveStructureInitialProposal,
+    AdditiveStructureTarget
+)
+
+
+@pytest.mark.parametrize(
+    "ad,expected",
+    [(AdditiveStructure([[1, 2, 3, 4, 5]]), 1),
+     (AdditiveStructure([[1, 2], [3, 4, 5]]), 2),
+     (AdditiveStructure([[1, 2, 3], [4, 5]]), 2),
+     (AdditiveStructure([[1], [2], [3], [4], [5]]), 5)]
+)
+def test_additive_structure_proposal_norm(ad, expected):
+    x = AdditiveStructureProposal.norm(ad)
+    assert x == expected
+
+
+@pytest.mark.parametrize(
+    "ad_a,ad_b,expected",
+    [(AdditiveStructure([[1, 2, 3, 4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]]), True),
+     (AdditiveStructure([[1, 2], [3, 4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]]), False),
+     (AdditiveStructure([[1, 2], [3], [4, 5]]), AdditiveStructure([[1, 2], [3], [4, 5]]), False),
+     (AdditiveStructure([[1], [2], [3], [4], [5]]), AdditiveStructure([[1], [2], [3], [4, 5]]), True)]
+)
+def test_additive_structure_proposal_heuristic(ad_a, ad_b, expected):
+    a = AdditiveStructureProposal.norm(ad_a)
+    b = AdditiveStructureProposal.norm(ad_b)
+    x = AdditiveStructureProposal.heuristic(a, b)
+    y = AdditiveStructureProposal.heuristic(b, a)
+    assert x == expected and y == expected
+
+
+@pytest.mark.parametrize(
+    "ad_start,ad_end,expected",
+    [(AdditiveStructure([[1, 2, 3, 4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]]), np.log(1/15)),
+     (AdditiveStructure([[1, 2], [3, 4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]]), -np.inf),
+     (AdditiveStructure([[1, 2], [3], [4, 5]]), AdditiveStructure([[1, 2], [3], [4, 5]]), -np.inf),
+     (AdditiveStructure([[1], [2], [3], [4], [5]]), AdditiveStructure([[1], [2], [3], [4, 5]]), np.log(0.1))]
+)
+def test_additive_structure_proposal_eval(ad_start, ad_end, expected):
+    prop = AdditiveStructureProposal(ad_start)
+    logprob = prop.eval(ad_end)
+    np.testing.assert_almost_equal(logprob, expected)
+
+
+@pytest.mark.parametrize(
+    "seed,ad_start,expected",
+    [(0, AdditiveStructure([[1, 2, 3, 4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]])),
+     (1, AdditiveStructure([[1, 2], [3, 4, 5]]), AdditiveStructure([[1, 2], [3], [4, 5]])),
+     (2, AdditiveStructure([[1, 2], [3], [4, 5]]), AdditiveStructure([[1, 2, 3], [4, 5]])),
+     (3, AdditiveStructure([[1], [2], [3], [4], [5]]), AdditiveStructure([[1], [2], [3, 5], [4]]))]
+)
+def test_additive_structure_proposal_sample(seed, ad_start, expected):
+    prop = AdditiveStructureProposal(ad_start, rng=RNG(seed))
+    sampled = prop.sample()
+    assert sampled == expected
 
 
 @pytest.mark.parametrize(
