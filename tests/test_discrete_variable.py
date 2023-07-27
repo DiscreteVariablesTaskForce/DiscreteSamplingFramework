95306d2dbefc58c94d202336ef74905e494feebc
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-07-27 17:55:43 +0100

    test: add test for generic encode/decode
    Add test of encode, decode of generic discrete variable

---
+++ tests/test_discrete_variable.py
@@ -0,0 +1,21 @@
+import pytest
+from discretesampling.base.types import DiscreteVariable
+
+
+class ExampleParticleClass(DiscreteVariable):
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+
+@pytest.mark.parametrize(
+    "x",
+    [(ExampleParticleClass(1)),
+     (ExampleParticleClass(10))]
+)
+def test_encode_decode_generic_type(x):
+    encoded_x = ExampleParticleClass.encode(x)
+    decoded_x = ExampleParticleClass.decode(encoded_x, x)
+    assert x == decoded_x
