test: fix numerical issues in tests/test_smc_components.py

tests/test_smc_components.py
@@ -23,7 +23,7 @@ def split_across_cores(N, P, rank):
 )
 def test_ess(logw, expected_ess):
     calc_ess = ess(logw, exec=Executor())
-    np.testing.assert_almost_equal(calc_ess, expected_ess)  # use almost_equal for numerical inaccuracy
+    np.testing.assert_allclose(calc_ess, expected_ess)  # use almost_equal for numerical inaccuracy
 
 
 @pytest.mark.parametrize(
@@ -49,8 +49,9 @@ def test_systematic_resampling(particles, logw, expected):
     N = len(particles)
     new_particles, new_logw = systematic_resampling(particles, logw, rng=RNG(1), exec=Executor())
     values_equal = all(x == y for x, y in zip(new_particles, expected))
-    weights_equal = all(new_logw == np.full((N,), -np.log(N)))
-    assert values_equal and weights_equal
+    expected_logw = np.full((N,), -np.log(N))
+    assert values_equal
+    np.testing.assert_almost_equal(new_logw, expected_logw)
 
 
 @pytest.mark.parametrize(