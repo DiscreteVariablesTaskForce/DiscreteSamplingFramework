3c473834e84554e74524b84cba1c66655f2449f3
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-08-21 14:06:08 +0100

    style: exclude __init__ imports from linting

--- discretesampling/domain/decision_tree/__init__.py
+++ discretesampling/domain/decision_tree/__init__.py
@@ -3,5 +3,5 @@ from .tree_distribution import TreeProposal, forward, reverse  # noqa
 from .tree_initial_proposal import TreeInitialProposal  # noqa
 from .tree_target import TreeTarget  # noqa
 from .metrics import stats, accuracy, calculate_leaf_occurences   # noqa
-from .regression_tree_target import RegressionTreeTarget
-from .regression_metrics import RegressionStats, accuracy_mse, regression_likelihood
+from .regression_tree_target import RegressionTreeTarget  # noqa
+from .regression_metrics import RegressionStats, accuracy_mse, regression_likelihood  # noqa
