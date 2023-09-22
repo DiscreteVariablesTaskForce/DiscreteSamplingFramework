e6374a6139f5b359ddf42276dd01f9e16fa9893b
Alex Phillips <A.M.Phillips@liverpool.ac.uk>
2023-09-22 14:38:13 +0100

    fix: smc sampler example
    Change to not use optimal L and run for few iterations for example

--- examples/smc_example.py
+++ examples/smc_example.py
@@ -20,15 +20,13 @@ b = 5
 target = dt.TreeTarget(a, b)
 initialProposal = dt.TreeInitialProposal(X_train, y_train)
 
-# Necessary to use multiprocessing
-if __name__ == "__main__":
-    dtSMC = DiscreteVariableSMC(dt.Tree, target, initialProposal,
-                                use_optimal_L=True, exec=Executor())
+dtSMC = DiscreteVariableSMC(dt.Tree, target, initialProposal,
+                            use_optimal_L=False, exec=Executor())
 
-    try:
-        treeSamples = dtSMC.sample(10, 500)
-        smcLabels = dt.stats(treeSamples, X_test).predict(X_test, use_majority=True)
-        smc_acc = dt.accuracy(y_test, smcLabels)
-        print(numpy.mean(smc_acc))
-    except ZeroDivisionError:
-        print("SMC sampling failed due to division by zero")
+try:
+    treeSamples = dtSMC.sample(10, 10)
+    smcLabels = dt.stats(treeSamples, X_test).predict(X_test, use_majority=True)
+    smc_acc = dt.accuracy(y_test, smcLabels)
+    print(numpy.mean(smc_acc))
+except ZeroDivisionError:
+    print("SMC sampling failed due to division by zero")
