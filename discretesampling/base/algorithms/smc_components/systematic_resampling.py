192d90f3b3c5c4faf401851ba67025a18a86bda7
Soodeh <setup@Soodehs.local>
2024-02-05 14:21:20 +0000

    Fixing seed with mvrs_rng

--- discretesampling/base/algorithms/smc_components/systematic_resampling.py
+++ discretesampling/base/algorithms/smc_components/systematic_resampling.py
@@ -3,8 +3,6 @@ import numpy as np
 def systematic_resampling(x, w, mvrs_rng):
     
     N = len(w)
-    #x_new = np.zeros_like(x)
-    # x_new = [None] * N
     x_new = []
     w_new = np.ones_like(w) / N
     log_w_new = np.log(w_new)
@@ -12,8 +10,8 @@ def systematic_resampling(x, w, mvrs_rng):
 
     cw = np.cumsum(w)
     
-    u = np.random.rand()
-    #u = 0.0
+    #u = np.random.rand()
+    u = mvrs_rng.uniform()
     
     for i in range(N):
         j = 0
@@ -21,8 +19,6 @@ def systematic_resampling(x, w, mvrs_rng):
         while cw[j] < (i + u) / N:
             j += 1
 
-        # x_new[i] = x[j]
         x_new.append(x[j])
-        #p_i[i] = j
 
     return x_new, log_w_new
