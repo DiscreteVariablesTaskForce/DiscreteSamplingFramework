a5a5e3bc28c320d467f012728f1b3b998366f758
setup <soodeh.habibi92@gmail.com>
2024-03-25 10:19:35 +0000

    First commit on Minimum Error Continuous State Importance Resampling

--- discretesampling/base/algorithms/smc_components/minError_ImportanceResampling.py
+++ discretesampling/base/algorithms/smc_components/minError_ImportanceResampling.py
@@ -1,6 +1,6 @@
 import numpy as np
 from discretesampling.base.algorithms.smc_components.importance_resampling_version3 import systematic_resampling
-def importance_resampling_v3(x, w, mvrs_rng , N=None):
+def min_error_importance_resampling(x, w, mvrs_rng , N=None):
 
     if N is None:
         N = len(w)
