Bug fix in get_probdict that was causing computation of weighted jumps to hang
Add appropriate gather/scatter functions to improve parallel computation efficiency

Set up main_barkla.py file  to run on Barkla HPC and produce results with test files

discretesampling/domain/gaussian_mixture/main_barkla.py
@@ -1,10 +1,8 @@
-import joblib
+import numpy as np
+import math
+import matplotlib.pyplot as plt
 
-import sys
-sys.path.append("C:/Users/mattb242/desktop/projects/reversible_jump/local_code/DiscreteSamplingFramework")
-sys.path.append("C:/Users/mattb242/desktop/projects/reversible_jump/local_code/SMCComponents")
-import discretesampling
-
+from discretesampling.base.algorithms.smc_components.normalisation import normalise
 from discretesampling.domain.gaussian_mixture.mix_model_structure import Gaussian_Mix_Model
 from discretesampling.domain.gaussian_mixture.mix_model_initial_proposal import UnivariateGMMInitialProposal
 from discretesampling.domain.gaussian_mixture.parallel_gmm_smc import straight_SMC_MPI
@@ -23,12 +21,192 @@ def read_floats_from_file(filepath):
 
     return floats
 
+def MC_get_all_k(comps):
+    compdict = {}
+    for i in comps:
+        if i.Gaussian_Mix_Model.k in compdict.keys():
+            compdict[i.Gaussian_Mix_Model.k].append(i)
+        else:
+            compdict[i.Gaussian_Mix_Model.k] = [i]
+
+    avcompdict = {}
+    countcompdict = {}
+    for i in compdict:
+        countcompdict[i] = len(compdict[i])
+        avcompdict[i] = Gaussian_Mix_Model(np.average(np.array([j.Gaussian_Mix_Model.components for j in compdict[i]]), axis = 1))
+
+    return avcompdict, countcompdict
+
+def get_average_k(parts):
+    return np.average([parts[i].Gaussian_Mix_Model.k for i in range(len(parts))])
+def get_wt_average_k(parts, wts):
+    normwts = np.exp(normalise(np.array(wts)))
+    ks = np.array([i.Gaussian_Mix_Model.k for i in parts])
+    return np.dot(normwts, ks)
+
+def get_wt_av_mean(parts, wts, index):
+
+    return sum([parts[i].Gaussian_Mix_Model.means[index]*math.exp(wts[i]) for i in range(len(wts))])
+
+def get_wt_av_bic(parts, wts):
+    normwts = normalise(np.array(wts))
+    bics = np.exp(np.array([i.bic() for i in parts]))
+    return np.dot(normwts, bics)
+
+def extract_only_k_components(parts, wts, k):
+    k_comps = []
+    k_wts = []
+    #print(f'wts: {wts}')
+    for i in range(len(parts)):
+        if parts[i].Gaussian_Mix_Model.k == k:
+            k_comps.append(parts[i].Gaussian_Mix_Model.components)
+            k_wts.append(wts[i])
+
+    if len(k_wts) > 0:
+       k_wts = normalise(np.array(k_wts))
+
+    return k_comps, k_wts
+
+def average_model_components(comps, wts):
+    if comps == []:
+        return 0
+    else:
+        av = np.zeros_like(comps[0])
+
+        for i in range(len(wts)):
+
+            av+= math.exp(wts[i])*np.array(comps[i])
+
+    return sorted(av, key = lambda x: x[0])
+
+def extract_all_k(partlist, wtlist, ess, k):
+    ness = [20] + ess
+    across_comps = []
+    filtered_ess = []
+    for i in range(len(partlist)):
+        #print(f'All weights: {wtlist}')
+
+        k_filter = extract_only_k_components(partlist[i], np.array(wtlist[i]), k)
+        if len(k_filter[0]) > 0:
+            across_comps.append(average_model_components(k_filter[0], k_filter[1]))
+            filtered_ess.append(ness[i])
+
+    prop_ess_norm = np.log(np.array(filtered_ess)/sum(filtered_ess))
+
+    final_average = average_model_components(across_comps, prop_ess_norm)
+
+    return final_average
+
+def all_k_pmf(parts):
+    d = {}
+    N = len(parts)*len(parts[0])
+    for i in parts:
+        for j in i:
+            k = j.Gaussian_Mix_Model.k
+            if k in d.keys():
+                d[k] += 1
+            else:
+                d[k] = 1
+
+    for i in d:
+        d[i] = d[i]/N
+
+    return d
+
+
 testdir = 'C:/Users/mattb242/Desktop/Projects/reversible_jump/results/toy_model'
 gal_test_data = read_floats_from_file('C:/Users/mattb242/Desktop/Projects/reversible_jump/test_data/enzyme.txt')
 toy_test = Gaussian_Mix_Model([[-8, 1, 0.3], [8, 1, 0.7]])
 toy_test_data = toy_test.sample(100)
 init_dist = UnivariateGMMInitialProposal(2, 0.2, 2, 1, 1, 10, toy_test_data)
 
-test_SMC =straight_SMC_MPI(init_dist, 32, 1000)
+test_out =wt_informed_RJSMC_MPI(init_dist, 32, 1000)
 
-joblib.dump(test_SMC, testdir + '/str_toy_32x1000.gz')
+compprob = all_k_pmf(test_out[0])
+
+for i in compprob.keys():
+    if compprob[i] > 0.1:
+
+        comps = extract_all_k(test_out[0], test_out[1], test_out[2], i)
+        print(comps)
+        gmix = Gaussian_Mix_Model(comps)
+        b = min(toy_test_data) - 5
+        f = max(toy_test_data) + 5
+        x = np.linspace(b, f, 500)
+        dat = [gmix.eval(j) for j in x]
+        bic = gmix.bic(toy_test_data)
+        plt.plot(x, dat, label = f'k = {i}, p={round(compprob[i], 3)}, BIC = {round(bic,3)}')
+        plt.hist(toy_test_data, bins = 40, density = True)
+
+plt.legend()
+plt.savefig(testdir + '/wt_toy_32x200_comps')
+plt.cla()
+
+
+kav = [get_wt_average_k(test_out[0][i], test_out[1][i]) for i in range(1,len(test_out[0]))]
+ess = test_out[2]
+bicav = [get_wt_av_bic(test_out[0][i], test_out[1][i]) for i in range(1,len(test_out[0]))]
+
+x = [i for i in range(len(kav))]
+plt.plot(x, kav)
+plt.xlabel('t')
+plt.ylabel('average k')
+plt.savefig(testdir + '/wt_toy_32x1000_avk')
+#plt.show()
+plt.cla()
+
+x = [i for i in range(len(kav))]
+plt.plot(x, bicav)
+plt.xlabel('t')
+plt.ylabel('BIC')
+plt.savefig(testdir + '/wt_toy_32x1000_bic')
+#plt.show()
+plt.cla()
+
+
+xe = [i for i in range(len(test_out[2]))]
+plt.plot(xe, test_out[2])
+plt.xlabel('t')
+plt.ylabel('ESS')
+plt.savefig(testdir + '/str_toy_32x1000_ess')
+#plt.show()
+plt.cla()
+
+def divide_and_calculate_proportions(values, N, k):
+    # Ensure that N is not greater than the length of the list
+    if N > len(values):
+        raise ValueError("N cannot be greater than the length of the list")
+
+    # Calculate the size of each sub-list
+    sublist_size = len(values) // N
+
+    # Divide the list into N sub-lists
+    sublists = [values[i * sublist_size : (i + 1) * sublist_size] for i in range(N)]
+
+    # Calculate the proportion of values in each sub-list that are above k
+    proportions = []
+    for sublist in sublists:
+        if len(sublist) > 0:  # Avoid division by zero
+            proportion = sum(1 for x in sublist if x < k) / len(sublist)
+            proportions.append(proportion)
+        else:
+            proportions.append(0)
+
+    # Handle any remaining elements not included due to integer division
+    remaining_elements = values[N * sublist_size:]
+    if remaining_elements:
+        proportion = sum(1 for x in remaining_elements if x > k) / len(remaining_elements)
+        proportions.append(proportion)
+
+    return proportions
+
+resampling_rate = divide_and_calculate_proportions(ess, 20, 16)
+print('Resampling rate per 50 steps:')
+print(resampling_rate)
+
+xres = [i*50 for i in range(len(resampling_rate))]
+plt.plot(xres[1:], resampling_rate[1:])
+plt.ylabel('Resampling rate per 50 steps')
+plt.xlabel('t')
+plt.savefig(testdir + '/str_toy_32x1000_resampling')
+plt.cla()

discretesampling/domain/gaussian_mixture/parallel_gmm_smc.py
@@ -10,6 +10,7 @@ from discretesampling.base.executor import Executor
 from discretesampling.base.algorithms.smc_components.effective_sample_size import ess
 from discretesampling.base.algorithms.smc_components.resampling import systematic_resampling
 from discretesampling.base.algorithms.smc_components.normalisation import normalise
+from discretesampling.base.util import pad, restore
 from discretesampling.domain.gaussian_mixture.mix_model_distribution import decode
 
 from discretesampling.domain.gaussian_mixture.mix_model_initial_proposal import UnivariateGMMInitialProposal
@@ -17,6 +18,7 @@ from discretesampling.domain.gaussian_mixture.mix_model_structure import Gaussia
 
 from mpi4py import MPI
 
+'''
 def pad(particles):
     mlen = max([i.Gaussian_Mix_Model.k for i in particles])
     return np.array([i.encode(mlen-i.Gaussian_Mix_Model.k) for i in particles])
@@ -24,6 +26,7 @@ def pad(particles):
 def restore(coded_particles):
     #print(f'decoding the following array: {coded_particles}')
     return np.array([decode(i) for i in coded_particles])
+'''
 
 def read_floats_from_file(filepath):
     floats = []
@@ -113,21 +116,19 @@ def SMCstep(particle, weight, grow_prob=[0.5, 0, 0.5]):
     return front, weight + acc_ratio
 
 
-def one_step_sample_MPI(x_list, y_list):
-    print('sample proposition now begins')
+def one_step_sample_MPI(particles, weights):
     sys.stdout.flush()
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
+    '''
     # Step 1: Divide the lists into chunks to distribute over processes
     n = len(x_list)
     chunk_size = n // size
     remainder = n % size
 
-    # Debug print: Check the division of work
-    if rank == 0:
-        print(f"Total elements: {n}, chunk size: {chunk_size}, remainder: {remainder}")
+
     sys.stdout.flush()
     # Determine the indices for the portion of the list each process will handle
     if rank < remainder:
@@ -138,48 +139,49 @@ def one_step_sample_MPI(x_list, y_list):
         local_end = local_start + chunk_size
 
     # Debug print: Check local range for each rank
-    print(f"Rank {rank}: local range {local_start} to {local_end}")
+
 
     # Step 2: Scatter the x_list and y_list segments to each process
     local_x = x_list[local_start:local_end]
     local_y = y_list[local_start:local_end]
 
-    # Debug print: Check the local chunks
-    #print(f"Rank {rank}: local_x = {local_x}, local_y = {local_y}")
 
     # Step 3: Each process computes func(x, y) for its segment, which now returns two values
+    
+    '''
     local_results1 = []
     local_results2 = []
-
-    for x, y in zip(local_x, local_y):
+    for x, y in zip(particles, weights):
 
         res1, res2 = SMCstep(x, y)
 
         local_results1.append(res1)
         local_results2.append(res2)
+
     # Barrier to ensure all processes have completed before gathering results
     comm.barrier()
+    #print(f'local results on rank {rank}: {local_results2}')
     sys.stdout.flush()
 
     # Step 4: Gather the results from all processes back at the root process
-    #print(f"Rank {rank}: Reached comm.gather with local_results1 = {local_results1}, local_results2 = {local_results2}")
     gathered_results1 = comm.gather(local_results1, root=0)
     gathered_results2 = comm.gather(local_results2, root=0)
     comm.barrier()
-    #print('gathering')
     sys.stdout.flush()
 
+    '''
     if rank == 0:
         # Flatten the gathered results to get the final result in the original order
         result1 = np.array([item for sublist in gathered_results1 for item in sublist])
         result2 = np.array([item for sublist in gathered_results2 for item in sublist])
 
-        # Debug print: Check the gathered results
-        #print(f"Rank {rank}: gathered results1 = {result1}, gathered results2 = {result2}")
 
         return result1, result2
     else:
         return None, None
+    '''
+
+    return local_results1, local_results2
 
 
 def get_k_indices(particles):
@@ -193,29 +195,42 @@ def get_k_indices(particles):
     return dict(sorted(inddict.items()))
 
 
-def get_probdict(particles, weights):
-    neff = ess(weights, exec=Executor_MPI())
-    inddict = get_k_indices(particles)
-    std = neff / (max(inddict.keys()) + 2)
+def get_probdict(particles, weights, neff):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
 
-    allprobs = []
-    for i in range(max(inddict.keys()) + 2):
-        if i == 0:
-            allprobs.append(10 ** -235)
-        elif i in inddict.keys():
-            allprobs.append(sum([np.exp(weights[j]) for j in inddict[i]]))
-        else:
-            allprobs.append(std)
+    if rank == 0:
+        #print('Computing ess')
+        #sys.stdout.flush()
+        #neff = ess(weights, exec=Executor_MPI())
+        #sys.stdout.flush()
+        #print(f'ess = {neff}')
+        #comm.barrier()
+        #sys.stdout.flush()
 
-    probdicts = {}
-    for i in range(1, max(inddict.keys()) + 1):
-        ps = np.array(
-            [max(10 ** -236, allprobs[i - 1]), max(10 ** -236, allprobs[i]), max(10 ** -236, allprobs[i + 1])])
-        probdicts[i] = ps / sum(ps)
+        inddict = get_k_indices(particles)
+        std = neff / (max(inddict.keys()) + 2)
 
-    print(f'jump probabilities are {probdicts}')
+        allprobs = []
+        for i in range(max(inddict.keys()) + 2):
+            if i == 0:
+                allprobs.append(10 ** -235)
+            elif i in inddict.keys():
+                allprobs.append(sum([np.exp(weights[j]) for j in inddict[i]]))
+            else:
+                allprobs.append(std)
 
-    return probdicts
+        probdicts = {}
+        for i in range(1, max(inddict.keys()) + 1):
+            ps = np.array([max(10 ** -236, allprobs[i - 1]), max(10 ** -236, allprobs[i]), max(10 ** -236, allprobs[i + 1])])
+            probdicts[i] = ps / sum(ps)
+
+        #print(f'jump probabilities are {probdicts}')
+
+        return probdicts
+
+    else:
+        return None
 
 
 def RJMCMC(init, T):
@@ -240,63 +255,44 @@ def RJMCMC(init, T):
     return path, k, bic
 
 
-def wt_informed_onestep_MPI(particles, logweights):
+def wt_informed_onestep_MPI(particles, logweights, neff):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
     # make local PMFs for all values of k available to all nodes
+
+
+    comm.barrier()
+    sys.stdout.flush()
+
+    all_ps = np.array(comm.gather(particles, root = 0)).ravel()
+    all_wts = np.array(comm.gather(logweights, root = 0)).ravel()
+    probdict = None
+
     if rank == 0:
-        probdict = get_probdict(particles, logweights)
-    else:
-        probdict = None
+        #print('getting the probability dictionary')
+        sys.stdout.flush()
+        probdict = get_probdict(all_ps, all_wts, neff)
 
-    comm.bcast(probdict, root=0)
+    probdict = comm.bcast(probdict, root = 0)
 
-    # Divide the lists into chunks to distribute over processes
-    n = len(particles)
-    chunk_size = n // size
-    remainder = n % size
-
-    # Determine the indices for the portion of the list each process will handle
-    if rank < remainder:
-        local_start = rank * (chunk_size + 1)
-        local_end = local_start + chunk_size + 1
-    else:
-        local_start = rank * chunk_size + remainder
-        local_end = local_start + chunk_size
-
-    # Step 2: Scatter the x_list and y_list segments to each process
-    local_parts = particles[local_start:local_end]
-    local_logwts = logweights[local_start:local_end]
-
+    #print(f'rank {rank}: probdict = {probdict}')
     # Step 3: Each process computes func(x, y) for its segment, which now returns two values
     local_results1 = []
     local_results2 = []
 
-    for x, y in zip(local_parts, local_logwts):
-        # print(f'growth probs are {probdict[x.Gaussian_Mix_Model.k]}')
+    for x, y in zip(particles, logweights):
         res1, res2 = SMCstep(x, y, grow_prob=probdict[x.Gaussian_Mix_Model.k])
         local_results1.append(res1)
         local_results2.append(res2)
 
-    # Step 4: Gather the results from all processes back at the root process
-    gathered_results1 = comm.gather(local_results1, root=0)
-    gathered_results2 = comm.gather(local_results2, root=0)
-
-    if rank == 0:
-        # Flatten the gathered results to get the final result in the original order
-        result1 = np.array([item for sublist in gathered_results1 for item in sublist])
-        result2 = np.array([item for sublist in gathered_results2 for item in sublist])
-        return np.array(result1), np.array(result2)
-        sys.stdout.flush()
-    else:
-        return None, None
-
+    return local_results1, local_results2
 
 def straight_SMC_MPI(init, N, T):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
 
     loc_n = int(N)
     # rank = self.exec.rank
@@ -305,144 +301,153 @@ def straight_SMC_MPI(init, N, T):
     mvrs_rng = RNG(seed)
         # rngs = [RNG(i + rank * loc_n + 1 + seed) for i in range(loc_n)]  # RNG for each particle
 
-    initialParticles = [init.get_initial_dist()] * N
-    current_particles = initialParticles
-    logWeights = np.array([-math.log(N)]*N)
+    current_particles = [init.get_initial_dist()] * int(N/size)
 
-    particle_path = [current_particles]
-    logwt_path = [logWeights]
+    logWeights = np.array([-math.log(N)]*int(N/size))
+    #print(f'rank = {rank}, weights = {logWeights}')
 
-    # display_progress_bar = verbose and rank == 0
-    # progress_bar = tqdm(total=Tsmc, desc="SMC sampling", disable=not display_progress_bar)
+    all_particles = np.array(comm.gather(current_particles, root=0)).ravel()
+    all_wts = np.array(comm.gather(logWeights, root=0)).ravel()
+
+    if rank == 0:
+        particle_path = [all_particles]
+        logwt_path = [all_wts]
+        ess_path = [N]
 
     sys.stdout.flush()
-    #current_particles = comm.bcast(current_particles, root=0)
-    #logWeights = comm.bcast(logWeights, root=0)
-
-    #front = comm.bcast(front, root=0)
-    #logwts_front = comm.bcast(logwts_front, root=0)
 
     for t in range(T):
 
         comm.barrier()
-        print(f'beginning step {t}')
-        #print(f'Current particles on rank {rank}: {current_particles}')
+        if rank == 0:
+            print(f'beginning step {t}')
         sys.stdout.flush()
 
         logWeights = normalise(logWeights, exec = Executor_MPI())
-        #print(f'normalised logWeights: {logWeights}')
         neff = ess(logWeights, exec = Executor_MPI())
+        print(f'rank {rank}: neff = {neff}')
+
+        if rank == 0:
+            ess_path.append(neff)
 
         if math.log(neff) < math.log(N) - math.log(2):
-            print(f'resampling at step {t} because ESS = {neff}')
-            #rint(f'Current_particles at rank {rank}, are {current_particles}')
 
-            current_particles = pad(current_particles)
+            current_particles = pad(current_particles, exec = Executor_MPI())
             current_particles, logWeights = systematic_resampling(
                 current_particles, logWeights, mvrs_rng, exec = Executor_MPI())
-            #current_particles = comm.bcast(current_particles, root=0)
+            comm.barrier()
+
+
             current_particles = restore(current_particles)
-
-            #print(f'post-resampling Logweights are {logWeights}')
-
-
-        new_particles = copy.copy(current_particles)
 
         sys.stdout.flush()
 
-        # Propose new samples from the particle front in a single step
-
-        #current_particles, logWeights = one_step_sample_MPI(new_particles, logWeights)
-
-
-        prop_front = []
-        prop_wts = []
-        for i in range(len(current_particles)):
-            i_front, i_wt = SMCstep(new_particles[i], logWeights[i])
-            prop_front.append(i_front)
-            prop_wts.append(i_wt)
-
-        print(f'Component Lengths: {[i.Gaussian_Mix_Model.k for i in prop_front]}')
-
-        # retain sample
-
-        particle_path.append(np.array(prop_front))
-        logwt_path.append(np.array(prop_wts))
-
-        print(f'Path length {len(particle_path)}')
-
-        current_particles = prop_front
-        logWeights = prop_wts
+        current_particles, logWeights = one_step_sample_MPI(current_particles, logWeights)
+        #print(f'rank = {rank}, after sample weights = {logWeights}')
 
 
-    return particle_path, logwt_path
+
+        all_particles = np.array(comm.gather(current_particles, root = 0)).ravel()
+        all_wts = np.array(comm.gather(logWeights, root = 0)).ravel()
+
+        if rank == 0:
+            particle_path.append(np.array(all_particles))
+            logwt_path.append(np.array(all_wts))
+
+        t+=1
+
+
+    return particle_path, logwt_path, ess_path
 
 
 def wt_informed_RJSMC_MPI(init, N, T):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+
+    loc_n = int(N)
+    # rank = self.exec.rank
+
+    seed = 0
+    mvrs_rng = RNG(seed)
+    # rngs = [RNG(i + rank * loc_n + 1 + seed) for i in range(loc_n)]  # RNG for each particle
+
+    current_particles = [init.get_initial_dist()] * int(N / size)
+
+    logWeights = np.array([-math.log(N)] * int(N / size))
+    # print(f'rank = {rank}, weights = {logWeights}')
+
+
+    all_particles = np.array(comm.gather(current_particles, root=0)).ravel()
+    all_wts = np.array(comm.gather(logWeights, root=0)).ravel()
     sys.stdout.flush()
-    t = 0
-    n = 0
 
-    eff_ss = []
+    if rank == 0:
+        #print(f'gathered weights: {all_wts}')
+        particle_path = [all_particles]
+        logwt_path = [all_wts]
+        ess_path = [N]
 
-    # Initialise the particles and weights
-    current_particles = []
-    while n < N:
-        current_particles.append(init.get_initial_dist())
-        n += 1
-    logWeights = np.array([-math.log(N)] * N)
-    norm_est = [-np.log(N)]
+    comm.barrier()
+    #print(f'logWeights at rank {rank} after scattering:{logWeights}')
 
-    particle_path = [current_particles]
-    logwt_path = [logWeights]
+    sys.stdout.flush()
 
-    print(f'Beginning step {t}')
-    while t < T:
+    for t in range(T):
 
-        print(f'Beginning step {t}')
+        comm.barrier()
+        if rank == 0:
+            print(f'beginning step {t}')
+            sys.stdout.flush()
+
+        logWeights = normalise(logWeights, exec=Executor_MPI())
+        #print(f'rank = {rank}, normalised logWeights = {logWeights}')
         sys.stdout.flush()
-
-        # Extract latest normalising constant estimate
-        norm_t = np.log(sum(np.exp(logwts_front))) - math.log(N)
-        norm_est.append(norm_t)
-        print(f'logZ = {norm_est[-1]}')
-        sys.stdout.flush()
-
-        normed_logwts_front = normalise(logwts_front, exec=Executor_MPI())
-        sys.stdout.flush()
-
         # check ESS of sampling front, and resample if necessary
-        neff = ess(normed_logwts_front, exec=Executor_MPI())
-        eff_ss.append(neff)
-        print(f'Effective sample size is {neff}')
+        neff = ess(logWeights, exec=Executor_MPI())
+        if rank == 0:
+            ess_path.append(neff)
+            #print(f'Effective sample size is {neff}')
+
         sys.stdout.flush()
 
         if math.log(neff) < math.log(N) - math.log(2):
-            #coded_front = pad(current_particles)
-            coded_front, logwts_front = systematic_resampling(
-                coded_front, normed_logwts_front, rng=RNG(), exec=Executor_MPI())
-            #front = restore(coded_front, current_particles)
+            current_particles = pad(current_particles, exec=Executor_MPI())
+            current_particles, logWeights = systematic_resampling(
+                current_particles, logWeights, mvrs_rng, exec=Executor_MPI())
+            comm.barrier()
+
+            current_particles = restore(current_particles)
 
         sys.stdout.flush()
 
         # Propose new samples from the particle front in a single step
 
-        front, logwts_front = wt_informed_onestep_MPI(front, logwts_front)
-        front = comm.bcast(front, root=0)
-        logwts_front = comm.bcast(logwts_front, root=0)
-        print(f'logwts front:{logwts_front}')
+        sys.stdout.flush()
+
+        #prop_front = comm.bcast(prop_front, root=0)
+        #prop_wts = comm.bcast(prop_wts, root=0)
 
         # retain sample
-        particle_path.append(front)
-        logwt_path.append(logwts_front)
-        print(f'weights of the new particles: {logwts_front}')
+        #print('Doing new sample')
+        current_particles, logWeights = wt_informed_onestep_MPI(current_particles, logWeights, neff)
+        sys.stdout.flush()
+        all_particles = np.array(comm.gather(current_particles, root=0)).ravel()
+        all_Weights = np.array(comm.gather(logWeights, root=0)).ravel()
+
+        if rank == 0:
+            particle_path.append(all_particles)
+            logwt_path.append(all_Weights)
+
+        #print(f'logWeights at rank {rank}:{logWeights}')
+        comm.barrier()
+
         sys.stdout.flush()
 
         t += 1
-
-    return particle_path, logwt_path, eff_ss, norm_est
+    if rank == 0:
+        return particle_path, logwt_path, ess_path
+    else:
+        return None, None, None
 
 