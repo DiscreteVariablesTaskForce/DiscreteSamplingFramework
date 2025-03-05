Compute exact Jacobian for jump moves using Sympy. Fix weight normalisation bug for birth move

discretesampling/domain/gaussian_mixture/mix_model_distribution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sy
 import math
 import copy
 from scipy.stats import norm
@@ -11,6 +12,7 @@ from scipy.stats import invgamma
 from scipy.special import logsumexp
 from pickle import dumps
 from pickle import loads
+from mpi4py import MPI
 
 import sys
 sys.path.append('C:/Users/mattb242/Desktop/Projects/reversible_jump/local_code/DiscreteSamplingFramework')
@@ -132,18 +134,25 @@ class GMM_Distribution():
         self.last_move = None
         self.aux_rand = None
 
-    def encode(self,pad):
+    def encode(self):
+        lmove_list = ['split, merge, birth, death']
+        if self.last_move in lmove_list:
+            move_code = lmove_list.index(self.last_move)
+        else:
+            move_code = 4
         dat_array = self.Data_Allocation.all_data()
-        print(f'Data length: {len(dat_array)}')
+        #print(f'Data length: {len(dat_array)}')
         alloc_array = self.Data_Allocation.all_allocations()
-        print(f'Allocation length: {len(alloc_array)}')
-        pad_comps = [self.Gaussian_Mix_Model.k] + list(self.Gaussian_Mix_Model.means) + list(self.Gaussian_Mix_Model.vars) + list(self.Gaussian_Mix_Model.wts)
+        #print(f'Allocation length: {len(alloc_array)}')
+        #print(f'Comps to encode {self.Gaussian_Mix_Model.components}')
+        pad_comps = list(self.Gaussian_Mix_Model.means) + list(self.Gaussian_Mix_Model.vars) + list(self.Gaussian_Mix_Model.wts)
+        #print(f'Comp length: {len(pad_comps)}')
         params = [self.la, self.delta, self.alpha, self.g, self.ep_h, self.ep_k]
 
-        all_mix = list(dat_array)+list(alloc_array)+pad_comps+params + [0]*(pad) + [pad]
+        all_mix = [move_code, len(dat_array), self.Gaussian_Mix_Model.k] +(dat_array)+list(alloc_array)+pad_comps+params
         all = [float(x) for x in all_mix]
 
-        print(f'everything is {len(all)}')
+        #print(f'everything for {self.Gaussian_Mix_Model.k} is {len(all)}')
 
         return np.array(all)
 
@@ -301,8 +310,11 @@ class GMM_Distribution():
 
         insertion = self.Gaussian_Mix_Model.insert_new_component([nmu, nvar, nwt])
         s = sum(insertion[0].wts)
-        for i in insertion[0].components:
-            i[2] = i[2]/s
+        for i in range(len(insertion[0].components)):
+            if i != insertion[1]:
+                insertion[0].components[i][2] = insertion[0].components[i][2]*(1-nwt)
+
+        #print(f'Checking weight sum for birth = {sum(insertion[0].wts)}')
 
         new_gmm = insertion[0]
 
@@ -395,16 +407,16 @@ class GMM_Distribution():
     def var_gibbs_update(self):
 
         newcomps = []
-        for i in self.Gaussian_Mix_Model.components:
-            idat = self.Data_Allocation.allocation[self.Gaussian_Mix_Model.components.index(i)]
+        for i in range(self.Gaussian_Mix_Model.k):
+            idat = self.Data_Allocation.allocation[i]
             n_i = len(idat)
             if n_i == 0:
                 var_i = 0
             else:
-                var_i = sum([(j-i[0])**2 for j in idat])
+                var_i = sum([(j-self.Gaussian_Mix_Model.components[i][0])**2 for j in idat])
 
             new_s_i = invgamma.rvs(self.alpha + (n_i/2), scale = self.beta + (var_i/2))
-            newcomps.append([i[0], new_s_i, i[2]])
+            newcomps.append([self.Gaussian_Mix_Model.components[i][0], new_s_i, self.Gaussian_Mix_Model.components[i][2]])
 
         newmodel = Gaussian_Mix_Model(newcomps)
         new_dist = GMM_Distribution(newmodel, self.Data_Allocation, self.la, self.delta, self.alpha, self.g, self.ep_h,
@@ -562,12 +574,24 @@ class GMM_Distribution():
                 palloc += math.log(normed_split_weights[1]) + norm.logpdf(i, splits[1][0], np.sqrt(splits[1][1]))
 
             #compute jacobian of split function
-            J = (sc[2]*np.abs(splits[1][0] - splits[0][0])*splits[1][1] +splits[0][1]) /((
-                sc[1]*(1 - (us[1] ** 2))*us[2] *(1 - us[2])))
 
-            p_xy = ev + priors
+            m, s, w, u_1, u_2, u_3 = sy.symbols('m, s, w, u_1, u_2, u_3')
+            m_1, m_2, s_1, s_2, w_1, w_2, = sy.symbols('m_1, m_2, s_1, s_2, w_1, w_2')
+            q = sy.Matrix([m + u_2 * (s * (sy.sqrt(w_2 / w_1))), m - u_2 * (s * (sy.sqrt(w_1 / w_2))),
+                           sy.sqrt(u_3 * (1 - u_2 ** 2) * s * (w / w_1)),
+                           sy.sqrt((1 - u_3) * (1 - u_2 ** 2) * s * (w / w_2)),
+                           u_1 * w, (1 - u_1) * w])
 
-            r_x = qprob + palloc + math.log(split_prob) - math.log(J)
+            J_sym = q.jacobian([m, s, w, u_1, u_2, u_3])
+            J_mat = J_sym.subs({m:sc[0], s:np.sqrt(sc[1]), w:sc[2], u_1:us[0], u_2:us[1], u_3:us[2], w_1:splits[0][2], w_2:splits[1][2]})
+            J = np.abs(J_mat.det())
+
+            #J = (sc[2]*np.abs(splits[0][0] - splits[1][0])*splits[1][1]*splits[0][1]) /((
+                #sc[1]*(1 - (us[1] ** 2))*us[2] *(1 - us[2])))
+
+            p_xy = ev + priors + math.log(J) + math.lgamma(self.Gaussian_Mix_Model.k + 1)
+
+            r_x =  qprob + palloc + math.log(split_prob)
 
             return p_xy, r_x
 
@@ -575,10 +599,10 @@ class GMM_Distribution():
 
         # check if current distribution could have been derived from previous distribution by a split at all
         if previous.Gaussian_Mix_Model.k - self.Gaussian_Mix_Model.k != 1:
-            raise Exception('Merge Eval Error: More than one extra component in previous distribution')
+            raise Exception(f'Merge Eval Error: More than one extra component in previous distribution')
         elif previous.Gaussian_Mix_Model.k > 2 and util.matchlist(previous.Gaussian_Mix_Model.means,
                                            self.Gaussian_Mix_Model.means) != 2:
-            raise Exception('Merge Eval Error: Distributions differ at more than two parameter vectors')
+            raise Exception(f'Merge Eval Error: Distributions {previous.Gaussian_Mix_Model.means} and {self.Gaussian_Mix_Model.means} differ at more than two parameter vectors')
         # if previous distribution is compatible, find the  merge index
         else:
             ind = 0
@@ -600,10 +624,13 @@ class GMM_Distribution():
             # compute current priors p(k), p(mu), p(sigma) and p(w)
             priors = sum([i for i in self.compute_parameter_priors()])
 
-            p_xy = ev + priors
+            J = 1
+            qprob = 1
+
+            p_xy = ev + priors + math.log(J) + math.lgamma(self.Gaussian_Mix_Model.k + 1)
 
             #Jump is deterministic, so probability is just raw jump probability for merge
-            r_x = math.log(mergeprob)
+            r_x = math.log(qprob) + math.log(mergeprob)
 
 
             return p_xy, r_x
@@ -629,8 +656,11 @@ class GMM_Distribution():
             # compute current priors p(k), p(mu), p(sigma) and p(w)
             priors = sum([i for i in self.compute_parameter_priors()])
 
-            p_xy = ev + priors
-            r_x = math.log(birthprob) + beta.logpdf(sc[2], 1, self.Gaussian_Mix_Model.k) - (previous.Gaussian_Mix_Model.k*math.log(1-sc[2]))
+            qprob = beta.pdf(sc[2], 1, self.Gaussian_Mix_Model.k)
+            J = (1-sc[2])**self.Gaussian_Mix_Model.k
+
+            p_xy = ev + priors + math.log(J)
+            r_x = math.log(qprob) + math.log(birthprob)
 
         return p_xy, r_x
 
@@ -649,8 +679,12 @@ class GMM_Distribution():
         # compute current priors p(k), p(mu), p(sigma) and p(w)
         priors = sum([i for i in self.compute_parameter_priors()])
 
-        p_xy = ev + priors
-        r_x = math.log(deathprob) - math.log(k_0)
+        qprob = 1/(k_0)
+        J = 1
+
+        p_xy = ev + priors + math.log(J)
+        r_x =  math.log(qprob) + math.log(deathprob)
+
         return p_xy, r_x
     def discrete_forward_eval(self, splitprob, mergeprob, birthprob, deathprob):
 
@@ -686,28 +720,45 @@ class GMM_Distribution():
 
 
 def decode(encoded_particle):
-    datlen = encoded_particle[-1]
-    justdat = encoded_particle[:datlen]
-    alloc = encoded_particle[datlen:2*datlen].astype(int)
-    alldict = {}
-    for i in range(len(justdat)):
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+
+    k = int(encoded_particle[2])
+    datlen = int(encoded_particle[1])
+    move_code = int(encoded_particle[0])
+    #print(f'Data length at rank {rank} is {datlen}')
+    justdat = encoded_particle[3:datlen+3]
+    alloc = encoded_particle[datlen+3:(2*datlen)+3]
+    alldict = {key:[] for key in range(k)}
+    for i in range(len(alloc)):
         if alloc[i] in alldict.keys():
-            alldict[i].append(justdat[i])
+            alldict[alloc[i]].append(justdat[i])
         else:
-            alldict[i] = [justdat[i]]
+            alldict[alloc[i]] = [justdat[i]]
 
     decoded_allocation = Data_Allocation(alldict)
 
-    k = encoded_particle[0]
-    means = encoded_particle[2*datlen:(2*datlen)+k]
-    vars = encoded_particle[(2*datlen)+k:(2*datlen)+(2*k)]
-    wts = encoded_particle[(2*datlen)+(2*k):(2*datlen)+(3*k)]
 
-    params = encoded_particle[(2*datlen)+(3*k):(2*datlen)+(3*k)+6]
+    means = encoded_particle[int((2*datlen)+3):int(((2*datlen)+3)+k)]
+    vars = encoded_particle[int(((2*datlen)+3)+k):int(((2*datlen)+3)+(2*k))]
+    wts = encoded_particle[int(((2*datlen)+3)+(2*k)):int(((2*datlen)+3)+(3*k))]
+
+    params = encoded_particle[int((2*datlen+3)+(3*k)):int((2*datlen+3)+(3*k)+6)]
+
+    #print(f'parameters - {params}')
+
 
     comps = [[means[i], vars[i], wts[i]] for i in range(len(means))]
+    #print(f'components - {comps}')
+    #print(f'decoded comps: {comps}')
     mix = Gaussian_Mix_Model(comps)
 
     decoded_particle = GMM_Distribution(mix, decoded_allocation, *params)
+    move_decode_list = ['split', 'merge', 'birth', 'death', 'other']
+
+    decoded_particle.last_move = move_decode_list[move_code]
+
 
     return decoded_particle
+
+

discretesampling/domain/gaussian_mixture/parallel_gmm_smc.py
@@ -28,6 +28,7 @@ def restore(coded_particles):
     return np.array([decode(i) for i in coded_particles])
 '''
 
+
 def read_floats_from_file(filepath):
     floats = []
     with open(filepath, 'r') as f:
@@ -40,6 +41,15 @@ def read_floats_from_file(filepath):
 
     return floats
 
+def get_wt_average_k(parts, wts):
+    normwts = np.exp(normalise(np.array(wts)))
+    ks = np.array([i.Gaussian_Mix_Model.k for i in parts])
+    return np.dot(normwts, ks)
+
+def get_wt_av_bic(parts, wts):
+    normwts = np.exp(normalise(np.array(wts)))
+    bics = np.array([i.bic() for i in parts])
+    return np.dot(normwts, bics)
 
 def RJMCMC_Step(particle, grow_prob=[0.5, 0, 0.5]):
     # Propose new samples from the particle front
@@ -49,42 +59,89 @@ def RJMCMC_Step(particle, grow_prob=[0.5, 0, 0.5]):
 
     # Compute discrete probabilities
 
-    cont_move = -proposed_cont.continuous_forward_eval(front) + front.continuous_forward_eval(proposed_cont)
+    #cont_move = -proposed_cont.continuous_forward_eval(front) + front.continuous_forward_eval(proposed_cont)
     # print(f'cont move = {cont_move}')
 
     if front.last_move == 'split':
-        fwd = front.split_log_eval(proposed_cont, grow_prob[2])
-        back = proposed_cont.merge_log_eval(front, grow_prob[0])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.split_log_eval(proposed_cont, grow_prob[0])
+        back = proposed_cont.merge_log_eval(front, grow_prob[2])
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'merge':
-        fwd = front.merge_log_eval(proposed_cont, grow_prob[0])
-        back = proposed_cont.split_log_eval(front, grow_prob[2])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.merge_log_eval(proposed_cont, grow_prob[2])
+        back = proposed_cont.split_log_eval(front, grow_prob[0])
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'birth':
-        fwd = front.birth_log_eval(proposed_cont, grow_prob[2])
-        back = proposed_cont.death_log_eval(front, grow_prob[0])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.birth_log_eval(proposed_cont, grow_prob[0])
+        back = proposed_cont.death_log_eval(front, grow_prob[2])
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'death':
-        fwd = front.death_log_eval(proposed_cont, grow_prob[0])
-        back = proposed_cont.birth_log_eval(front, grow_prob[2])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.death_log_eval(proposed_cont, grow_prob[2])
+        back = proposed_cont.birth_log_eval(front, grow_prob[0])
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     # elif front.last_move == 'stick':
     # acc_ratio = proposed_cont.eval()-front.eval() + cont_move
     else:
-        acc_ratio = 0
+        acc_ratio = 0 #front.eval() - particle.eval()
 
+    acc_prob = min(acc_ratio, 0)
     u = np.random.uniform(0, 1)
-    if u < math.exp(acc_ratio):
+    if u < math.exp(acc_prob):
         return front, True
     else:
         return particle, False
 
+def get_probdict(particles, weights, neff):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
 
-def SMCstep(particle, weight, grow_prob=[0.5, 0, 0.5]):
+    if rank == 0:
+        essprob = 1-(neff/len(particles))
+        print(f'neff = {neff/len(particles)}')
+        weights = normalise(weights)
+        #sys.stdout.flush()
+        #neff = ess(weights, exec=Executor_MPI())
+        #sys.stdout.flush()
+        #print(f'ess = {neff}')
+        #comm.barrier()
+        #sys.stdout.flush()
+
+        partsort = get_k_indices(particles)
+        inddict = partsort[0]
+        print(f'dictionary keys are {inddict.keys()}')
+        absent = partsort[1]
+        print(f'absent = {absent}')
+        if absent != 0:
+            std = essprob/absent
+        else:
+            std = 0
+
+        allprobs = []
+        for i in inddict:
+            if i == 0:
+                allprobs.append(0)
+            elif inddict[i] == []:
+                allprobs.append(std)
+            else :
+                allprobs.append((neff/len(particles))*sum([np.exp(weights[j]) for j in inddict[i]]))
+
+        allprobs = allprobs/sum(allprobs)
+        print(f'PMF is  {allprobs}')
+        probdicts = {}
+        for i in range(1, max(inddict.keys())):
+            ps = np.array([max(10**-235, allprobs[i - 1]), max(10**-235, allprobs[i]), max(10**-235, allprobs[i + 1])])
+            probdicts[i] = ps / sum(ps)
+
+        return probdicts
+
+    else:
+        return None
+
+def SMCstep(particle, weight, adj_probs = [[1/3, 1/3, 1/3], [1/3,1/3,1/3], [1/3,1/3,1/3]]):
+    #print(particle.last_move)
     # Propose new samples from the particle front
 
     proposed_cont = particle.continuous_forward_sample()
-    front = proposed_cont.discrete_forward_sample(move_pmf=grow_prob)
+    front = proposed_cont.discrete_forward_sample(move_pmf=adj_probs[1])
 
     # Compute discrete probabilities
 
@@ -92,28 +149,33 @@ def SMCstep(particle, weight, grow_prob=[0.5, 0, 0.5]):
     # print(f'cont move = {cont_move}')
 
     if front.last_move == 'split':
-        fwd = front.split_log_eval(proposed_cont, grow_prob[2])
-        back = proposed_cont.merge_log_eval(front, grow_prob[0])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.split_log_eval(proposed_cont, adj_probs[1][2])
+        back = proposed_cont.merge_log_eval(front, adj_probs[2][0])
+        #print(f'moves: {fwd[0] + fwd[1]}, {back[1] + back[0]}')
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'merge':
-        fwd = front.merge_log_eval(proposed_cont, grow_prob[0])
-        back = proposed_cont.split_log_eval(front, grow_prob[2])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.merge_log_eval(proposed_cont, adj_probs[0][2])
+        back = proposed_cont.split_log_eval(front, adj_probs[1][0])
+        #print(f'moves: {fwd[0] + fwd[1]}, {back[1] + back[0]}')
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'birth':
-        fwd = front.birth_log_eval(proposed_cont, grow_prob[2])
-        back = proposed_cont.death_log_eval(front, grow_prob[0])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.birth_log_eval(proposed_cont, adj_probs[2][0])
+        back = proposed_cont.death_log_eval(front, adj_probs[1][2])
+        #print(f'moves: {fwd[0] + fwd[1]}, {back[1] + back[0]}')
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     elif front.last_move == 'death':
-        fwd = front.death_log_eval(proposed_cont, grow_prob[0])
-        back = proposed_cont.birth_log_eval(front, grow_prob[2])
-        acc_ratio = (fwd[0] + back[1]) - (fwd[1] + back[0])
+        fwd = front.death_log_eval(proposed_cont, adj_probs[0][2])
+        back = proposed_cont.birth_log_eval(front, adj_probs[1][0])
+        #print(f'moves: {fwd[0] + fwd[1]}, {back[1] + back[0]}')
+        acc_ratio = (fwd[0] - fwd[1]) - (back[0] - back[1])
     # elif front.last_move == 'stick':
     # acc_ratio = proposed_cont.eval()-front.eval() + cont_move
     else:
-        acc_ratio = 0
+        acc_ratio = front.eval()-particle.eval()
 
-    # print(f'acceptance rate is {acc_ratio}')
-    return front, weight + acc_ratio
+    #print(f'acceptance rate is {acc_ratio}')
+    #print(f'acceptance ratio: {acc_ratio}')
+    return front, weight + min(0,acc_ratio)
 
 
 def one_step_sample_MPI(particles, weights):
@@ -185,14 +247,15 @@ def one_step_sample_MPI(particles, weights):
 
 
 def get_k_indices(particles):
-    inddict = {}
-    for i in particles:
-        if i.Gaussian_Mix_Model.k not in inddict.keys():
-            inddict[i.Gaussian_Mix_Model.k] = [list(particles).index(i)]
-        else:
-            inddict[i.Gaussian_Mix_Model.k].append(list(particles).index(i))
+    max_k = max((p.Gaussian_Mix_Model.k for p in particles), default=-1)
+    inddict = {k: [] for k in range(max_k + 2)}  # Preallocate dictionary with empty lists
 
-    return dict(sorted(inddict.items()))
+    for i, p in enumerate(particles):
+        inddict[p.Gaussian_Mix_Model.k].append(i)
+
+    absent = sum(1 for v in inddict.values() if not v)  # Count empty lists
+
+    return inddict, absent
 
 
 def get_probdict(particles, weights, neff):
@@ -200,7 +263,9 @@ def get_probdict(particles, weights, neff):
     rank = comm.Get_rank()
 
     if rank == 0:
-        #print('Computing ess')
+        essprob = 1-(neff/len(particles))
+        print(f'neff = {neff/len(particles)}')
+        weights = normalise(weights)
         #sys.stdout.flush()
         #neff = ess(weights, exec=Executor_MPI())
         #sys.stdout.flush()
@@ -208,24 +273,33 @@ def get_probdict(particles, weights, neff):
         #comm.barrier()
         #sys.stdout.flush()
 
-        inddict = get_k_indices(particles)
-        std = neff / (max(inddict.keys()) + 2)
+        partsort = get_k_indices(particles)
+        inddict = partsort[0]
+        print(f'dictionary keys are {inddict.keys()}')
+        absent = partsort[1]
+        print(f'absent = {absent}')
+        if absent != 0:
+            std = essprob/absent
+        else:
+            std = 0
 
         allprobs = []
-        for i in range(max(inddict.keys()) + 2):
+        for i in inddict:
             if i == 0:
-                allprobs.append(10 ** -235)
-            elif i in inddict.keys():
-                allprobs.append(sum([np.exp(weights[j]) for j in inddict[i]]))
-            else:
+                allprobs.append(0)
+            elif inddict[i] == []:
                 allprobs.append(std)
+            else :
+                allprobs.append((neff/len(particles))*sum([np.exp(weights[j]) for j in inddict[i]]))
 
-        probdicts = {}
-        for i in range(1, max(inddict.keys()) + 1):
-            ps = np.array([max(10 ** -236, allprobs[i - 1]), max(10 ** -236, allprobs[i]), max(10 ** -236, allprobs[i + 1])])
+        allprobs = allprobs/sum(allprobs)
+        print(f'PMF is  {allprobs}')
+        probdicts = {0:[0,0,0]}
+        for i in range(1, max(inddict.keys())):
+            ps = np.array([max(10**-235, allprobs[i - 1]), max(10**-235, allprobs[i]), max(10**-235, allprobs[i + 1])])
             probdicts[i] = ps / sum(ps)
 
-        #print(f'jump probabilities are {probdicts}')
+
 
         return probdicts
 
@@ -283,7 +357,7 @@ def wt_informed_onestep_MPI(particles, logweights, neff):
     local_results2 = []
 
     for x, y in zip(particles, logweights):
-        res1, res2 = SMCstep(x, y, grow_prob=probdict[x.Gaussian_Mix_Model.k])
+        res1, res2 = SMCstep(x, y, adj_probs =[probdict[x.Gaussian_Mix_Model.k-1], probdict[x.Gaussian_Mix_Model.k], [x.Gaussian_Mix_Model.k+1]])
         local_results1.append(res1)
         local_results2.append(res2)
 
@@ -313,6 +387,8 @@ def straight_SMC_MPI(init, N, T):
         particle_path = [all_particles]
         logwt_path = [all_wts]
         ess_path = [N]
+        k_path = [current_particles[0].Gaussian_Mix_Model.k]
+        bic_path = [current_particles[0].bic()]
 
     sys.stdout.flush()
 
@@ -330,7 +406,7 @@ def straight_SMC_MPI(init, N, T):
         if rank == 0:
             ess_path.append(neff)
 
-        if math.log(neff) < math.log(N) - math.log(2):
+        if math.log(neff) < math.log(N) + math.log(0.25): #math.log(N) - math.log(2):
 
             current_particles = pad(current_particles, exec = Executor_MPI())
             current_particles, logWeights = systematic_resampling(
@@ -340,12 +416,14 @@ def straight_SMC_MPI(init, N, T):
 
             current_particles = restore(current_particles)
 
+        if rank == 0:
+            k_path.append(get_wt_average_k(current_particles, logWeights))
+            bic_path.append(get_wt_av_bic(current_particles, logWeights))
+
         sys.stdout.flush()
 
         current_particles, logWeights = one_step_sample_MPI(current_particles, logWeights)
         #print(f'rank = {rank}, after sample weights = {logWeights}')
-
-
 
         all_particles = np.array(comm.gather(current_particles, root = 0)).ravel()
         all_wts = np.array(comm.gather(logWeights, root = 0)).ravel()
@@ -357,7 +435,7 @@ def straight_SMC_MPI(init, N, T):
         t+=1
 
 
-    return particle_path, logwt_path, ess_path
+    return particle_path, logwt_path, ess_path, k_path, bic_path
 
 
 def wt_informed_RJSMC_MPI(init, N, T):
@@ -372,7 +450,7 @@ def wt_informed_RJSMC_MPI(init, N, T):
     mvrs_rng = RNG(seed)
     # rngs = [RNG(i + rank * loc_n + 1 + seed) for i in range(loc_n)]  # RNG for each particle
 
-    current_particles = [init.get_initial_dist()] * int(N / size)
+    current_particles = [init.get_initial_dist()] * int(N/size)
 
     logWeights = np.array([-math.log(N)] * int(N / size))
     # print(f'rank = {rank}, weights = {logWeights}')
@@ -387,6 +465,8 @@ def wt_informed_RJSMC_MPI(init, N, T):
         particle_path = [all_particles]
         logwt_path = [all_wts]
         ess_path = [N]
+        k_path = [current_particles[0].Gaussian_Mix_Model.k]
+        bic_path = [current_particles[0].bic()]
 
     comm.barrier()
     #print(f'logWeights at rank {rank} after scattering:{logWeights}')
@@ -411,7 +491,7 @@ def wt_informed_RJSMC_MPI(init, N, T):
 
         sys.stdout.flush()
 
-        if math.log(neff) < math.log(N) - math.log(2):
+        if math.log(neff) < math.log(N) + math.log(0.25):
             current_particles = pad(current_particles, exec=Executor_MPI())
             current_particles, logWeights = systematic_resampling(
                 current_particles, logWeights, mvrs_rng, exec=Executor_MPI())
@@ -422,6 +502,9 @@ def wt_informed_RJSMC_MPI(init, N, T):
         sys.stdout.flush()
 
         # Propose new samples from the particle front in a single step
+        if rank == 0:
+            k_path.append(get_wt_average_k(current_particles, logWeights))
+            bic_path.append(get_wt_av_bic(current_particles, logWeights))
 
         sys.stdout.flush()
 
@@ -446,7 +529,7 @@ def wt_informed_RJSMC_MPI(init, N, T):
 
         t += 1
     if rank == 0:
-        return particle_path, logwt_path, ess_path
+        return particle_path, logwt_path, ess_path, k_path, bic_path
     else:
         return None, None, None
 