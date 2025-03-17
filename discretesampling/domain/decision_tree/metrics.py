09906b47414a74a84ecde310525d0b54c9a4fec8
EfthyvoulosDrousiotis <74777297+EfthyvoulosDrousiotis@users.noreply.github.com>
2025-03-17 12:05:53 +0000

    Add files via upload

--- discretesampling/domain/decision_tree/metrics.py
+++ discretesampling/domain/decision_tree/metrics.py
@@ -1,12 +1,18 @@
 import math
 import numpy as np
-import collections
+from collections import Counter
 import pandas as pd
 
 
+# MJAS fixed extremely confusing arg order!
 class stats():
     def __init__(self, trees, X_test):
+        print(len(trees))
         self.trees = trees
+        #self.X_train = X_train
+        #self.y_train = y_train
+        self.X_test = X_test
+        #self.y_test = y_test
 
     def getLeafPossibilities(self, x):
         target1, leafs_possibilities_for_prediction = calculate_leaf_occurences(x)
@@ -32,12 +38,17 @@ class stats():
 
     def predict(self, X_test, use_majority=True):
         all_labels_from_all_trees = []
+        all_possibilities_from_all_trees = []
+        leaf_possibilities_for_all_trees = []
+        leaf_predictions_for_all_trees = []# the leaf we ended up to make a prediction for one tree
         for tree in self.trees:
-            all_labels_from_this_trees = self.predict_for_one_tree(tree, X_test)
+            all_labels_from_this_trees, per_tree_possibilities, leaf_poss, leaf_pred = self.predict_for_one_tree(tree, X_test)
+            leaf_possibilities_for_all_trees.append(leaf_poss)
             all_labels_from_all_trees.append(all_labels_from_this_trees)
-
+            all_possibilities_from_all_trees.append(per_tree_possibilities)
+            leaf_predictions_for_all_trees.append(leaf_pred)
         if use_majority:
-            return self.majority_voting_predict(all_labels_from_all_trees)
+            return self.majority_voting_predict(all_labels_from_all_trees), all_possibilities_from_all_trees, leaf_possibilities_for_all_trees, leaf_predictions_for_all_trees
         else:
             return all_labels_from_all_trees
 
@@ -45,13 +56,16 @@ class stats():
         all_labels_for_this_tree = []
         leaf_possibilities = self.getLeafPossibilities(tree)
         leafs = sorted(tree.leafs)
+        all_probs_per_tree = []
+        leaf_predictions_for_one_tree = [] # the leaf we ended up to make a prediction for one tree
         for datum in X_test:
-            label_for_this_datum = self.predict_for_one_datum(tree, leafs, leaf_possibilities, datum)
+            label_for_this_datum, prob, leaf  = self.predict_for_one_datum(tree, leafs, leaf_possibilities, datum)
             all_labels_for_this_tree.append(label_for_this_datum)
-        return all_labels_for_this_tree
+            all_probs_per_tree.append(prob)
+            leaf_predictions_for_one_tree.append(leaf)
+        return all_labels_for_this_tree,all_probs_per_tree, leaf_possibilities, leaf_predictions_for_one_tree
 
     def predict_for_one_datum(self, tree, leafs, leaf_possibilities, datum):
-
         label = None
         flag = "false"
         current_node = tree.tree[0]
@@ -87,6 +101,7 @@ class stats():
                         leaf = current_node[1]
                         flag = "true"
                         indice = leafs.index(leaf)
+                        
                         for x, y in leaf_possibilities[indice].items():
                             if y == label_max:
                                 actual_label = 1
@@ -94,11 +109,10 @@ class stats():
                             if y > label_max:
                                 label_max = y
                                 actual_label = x
-
                         label = actual_label
                         break
-
-        return label
+        #print(label_max)
+        return label, label_max, leaf
 
 
 def accuracy(y_test, labels):
@@ -112,148 +126,102 @@ def accuracy(y_test, labels):
 
 
 # Π(Y_i|T,theta,x_i)
-def calculate_leaf_occurences(x):
-    '''
-    we calculate how many labelled as 0 each leaf has, how many labelled as 1
-    each leaf has and so on
-    '''
-    leaf_occurences = []
-    k = 0
-    for leaf in x.leafs:
-        leaf_occurences.append([leaf])
+# MJAS rewritten for efficiency and making corrections for hacked probabilities
+# ~30 times faster
+# just get counts - calculate likelihood as post processing
+# now uses the safe Dirichelet/Multinomial approach: p_j = (count_j + 1)/Sum_j(count_j +1)
+def improved_leaf_occurences(x, X_train, y_train, current_node_id = None): # set current_node = x.tree[0]
+    ### really need to index the tree!
+    if current_node_id is None:
+        current_node_id = x.tree[0][0]
+    #print("received", len(X_train))
+    if current_node_id in x.leafs:
+        # do actual calcs
+        #print("Process leaf", current_node_id)
+        return [(current_node_id, Counter(y_train))]
+    # otherwise .. use the threshold
+    for current_node in x.tree:
+        if current_node[0] == current_node_id:
+            break
+    #print("Process non-leaf", current_node)
+    thresh = current_node[4]
+    feat_num = current_node[3]
+    mask = X_train[:,feat_num] > thresh
+    occ = []
+    nr = np.sum(mask)
+    if nr < mask.shape[0]: # some go left
+        #print(" left child id = ", current_node[1])
+        occ_left = improved_leaf_occurences(x, X_train[~mask], y_train[~mask], current_node[1])
+        occ += occ_left # concatenate lists (of counters)
+    if nr > 0: # some go right
+        #print(" right child id = ", current_node[2])
+        occ_right = improved_leaf_occurences(x, X_train[mask], y_train[mask], current_node[2])
+        occ += occ_right
+    return(occ)
 
-    for datum in x.X_train:
-        flag = "false"
-        current_node = x.tree[0]
+# get expected and actual occupency for each decision node and leaf (in case we want proposals that are local to a subset)
+def node_occurences(x, X_train, expected = None, current_node_id = None):
+    if expected is None:
+        expected = float(X_train.shape[0])
+    if current_node_id is None:
+        current_node_id = x.tree[0][0]
+    if current_node_id in x.leafs:
+        return([(current_node_id, expected, len(X_train))])
+    # find this node
+    for current_node in x.tree:
+        if current_node[0] == current_node_id:
+            break
+    thresh = current_node[4]
+    feat_num = current_node[3]
+    mask = X_train[:,feat_num] > thresh
+    occ = [(current_node_id, expected, len(X_train))]
+    occ_left = node_occurences(x, X_train[~mask], expected * 0.5, current_node[1])
+    occ += occ_left # concatenate lists (of counters)
+    occ_right = node_occurences(x, X_train[mask], expected * 0.5, current_node[2])
+    occ += occ_right
+    return(occ)
 
-        # make sure that we are not in leafs. current_node[0] is the node
-        while current_node[0] not in x.leafs and flag == "false":
-            if datum[current_node[3]] > current_node[4]:
-                for node in x.tree:
-                    if node[0] == current_node[2]:
-                        current_node = node
-                        break
-                    if current_node[2] in x.leafs:
-                        leaf = current_node[2]
-                        flag = "true"
-                        break
 
-            else:
-                for node in x.tree:
-                    if node[0] == current_node[1]:
-                        current_node = node
-                        break
-                    if current_node[1] in x.leafs:
-                        leaf = current_node[1]
-                        flag = "true"
-                        break
-
-        '''
-        create a list of lists that holds the leafs and the number of
-        occurences for example [[4, 1, 1, 2, 2, 2], [5, 1, 1, 2, 2, 1],
-        [6, 1, 2, 2, 1, 2], [7, 2, 2, 2, 1, 2, 1, 2]]
-        The first number represents the leaf id number
-        '''
-
-        for item in leaf_occurences:
-
-            if item[0] == leaf:
-                item.append(x.y_train[k])
-        k += 1
-
-    '''
-    we have some cases where some leaf nodes may do not have any probabilities
-    because no data point ended up in the particular leaf
-    We add equal probabilities for each label to the particular leaf.
-    For example if we have 4 labels, we add 0:0.25, 1:0.25, 2:0.25, 3:0.25
-    '''
-
-    for item in leaf_occurences:
-        if len(item) == 1:
-            unique = set(x.y_train)
-            unique = list(unique)
-            for i in range(len(unique)):
-                item.append(i)
-
-    leaf_occurences = sorted(leaf_occurences)
-    leafs = sorted(x.leafs)
-
-    '''
-    we then delete the first number of the list which represents the leaf node
-    id.
-    '''
-    for i in range(len(leaf_occurences)):
-        new_list = True
-        for p in range(len(leaf_occurences[i])):
-            if new_list:
-                new_list = False
-                del leaf_occurences[i][p]
-
-    '''
-    first count the number of labels in each leaf.
-    Then create probabilities by normalising those values[0,1]
-    '''
-    leafs_possibilities = []
-    for number_of_leafs in range(len(leaf_occurences)):
-        occurrences = collections.Counter(leaf_occurences[number_of_leafs][:])
-        leafs_possibilities.append(occurrences)
-
-    # create leafs possibilities
-    for item in leafs_possibilities:
-        factor = 1.0/sum(item.values())
-        for k in item:
-            item[k] = item[k]*factor
-
-    product_of_leafs_probabilities = []
-    k = 0
-    for datum in x.X_train:
-        # print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        flag = "false"
-        current_node = x.tree[0]
-        # make sure that we are not in leafs. current_node[0] is the node
-        while current_node[0] not in leafs and flag == "false":
-            if datum[current_node[3]] > current_node[4]:
-                for node in x.tree:
-                    if node[0] == current_node[2]:
-                        current_node = node
-                        break
-                    if current_node[2] in leafs:
-                        leaf = current_node[2]
-                        # print(leaf)
-                        flag = "true"
-                        break
-
-            else:
-                for node in x.tree:
-                    if node[0] == current_node[1]:
-                        current_node = node
-                        break
-                    if current_node[1] in leafs:
-                        leaf = current_node[1]
-                        # print(leaf)
-                        flag = "true"
-                        break
-
-        if leaf in leafs:
-            # find the position of the dictionary probabilities given the leaf
-            # number
-            indice = leafs.index(leaf)
-            probs = leafs_possibilities[indice]
-            for prob in probs:
-                target_probability = probs[x.y_train[k]]
-
-                '''
-                we make sure that in the case we are on a homogeneous leaf,
-                we dont get a 0 probability but a very low one
-                '''
-
-                if target_probability == 0:
-                    target_probability = 0.02
-                if target_probability == 1:
-                    target_probability = 0.98
-
-            product_of_leafs_probabilities.append(math.log(target_probability))
-
-        k += 1
-    product_of_target_feature = np.sum(product_of_leafs_probabilities)
-    return product_of_target_feature, leafs_possibilities
+# MJAS efficient replacement for the existing function with the same name
+# now returns either log likelihood or accuracy depending on flag
+def calculate_leaf_occurences(x, X_test = None, y_test = None, pseudo_count = 1, categories = None, verbose = False, accuracy = False):
+    if categories is None:
+        categories = np.sort(np.unique(x.y_train)) # DANGER you should pass the categories in when working with subsets and pseudo count > 0
+    occ_dict = dict(improved_leaf_occurences(x, x.X_train, x.y_train))
+    # now get likelihood
+    # we can have DIFFERENT test data
+    if not X_test is None:
+        occ_dict_test = dict(improved_leaf_occurences(x, x.X_test, x.y_test))
+    else:
+        occ_dict_test = occ_dict
+    metric = 0.0    
+    total_test_count = 0
+    occ = []
+    if verbose:
+        print(x.leafs)
+        print(occ_dict)
+    for l in x.leafs:
+        regularised_counts = Counter(dict([(c, pseudo_count + 1e-12) for c in categories]))
+        if l in occ_dict.keys():
+            regularised_counts.update(occ_dict[l])
+        #test_counts = copy.deepcopy(regularised_counts)
+        #test_counts.subtract(pseudo_counts) # original counts but with zeros where needed 
+        rcval = np.array(list(regularised_counts.values()))
+        denom = np.sum(rcval)
+        probs = rcval / denom
+        #
+        if l in occ_dict_test.keys():
+            test_counts = occ_dict_test[l]
+        else:
+            test_counts = regularised_counts
+            test_counts.subtract(regularised_counts) # array of zeros
+        cval = np.array([test_counts[c] for c in regularised_counts.keys()])
+        total_test_count += np.sum(cval) # TO DO this only works if pseudo count > 0 so regularised counts is complete
+        if accuracy:
+            metric += np.sum(cval * probs)
+        else:
+            log_total_probs = cval * np.log(probs)
+            metric += np.sum(log_total_probs)       
+        #
+        occ.append(Counter(dict(zip(regularised_counts.keys(),probs)))) # just for backward compat
+    return((metric / total_test_count) if accuracy else metric, occ)
