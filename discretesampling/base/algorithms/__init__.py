385a755844a3996a774484c566a7917f73a1b597
HarviLehal <sma18hsl@liverpool.ac.uk>
2025-05-01 17:15:22 +0100

    file for hints added (need to change to actually be hints instead of mcmc as it is currently!!!) and added to __init__

--- discretesampling/base/algorithms/__init__.py
+++ discretesampling/base/algorithms/__init__.py
@@ -1,3 +1,4 @@
 from .mcmc import DiscreteVariableMCMC  # noqa
 from .smc import DiscreteVariableSMC  # noqa
+from .hints import DiscreteVariableHINTS  # noqa
 from .smc_components import ess, normalise, systematic_resampling  # noqa
---
+++ discretesampling/base/algorithms/hints.py
@@ -0,0 +1,58 @@
+import math
+import copy
+from tqdm.auto import tqdm
+from discretesampling.base.random import RNG
+
+
+class DiscreteVariableHINTS():
+
+    def __init__(self, variableType, target, initialProposal, proposal=None):
+        self.variableType = variableType
+        self.proposalType = variableType.getProposalType()
+        self.proposal = proposal
+        if proposal is None:
+            self.proposal = self.proposalType()
+        self.initialProposal = initialProposal
+        self.target = target
+
+    def sample(self, N, seed=0, verbose=True):
+        rng = RNG(seed)
+        initialSample = self.initialProposal.sample(rng)
+        current = initialSample
+
+        samples = []
+
+        display_progress_bar = verbose
+        progress_bar = tqdm(total=N, desc="MCMC sampling", disable=not display_progress_bar)
+
+        for i in range(N):
+            forward_proposal = self.proposal
+            proposed = forward_proposal.sample(current, rng=rng)
+
+            reverse_proposal = self.proposal
+
+            forward_logprob = forward_proposal.eval(current, proposed)
+            reverse_logprob = reverse_proposal.eval(proposed, current)
+
+            current_target_logprob = self.target.eval(current)
+            proposed_target_logprob = self.target.eval(proposed)
+
+            log_acceptance_ratio = proposed_target_logprob -\
+                current_target_logprob + reverse_logprob - forward_logprob
+            if log_acceptance_ratio > 0:
+                log_acceptance_ratio = 0
+            acceptance_probability = min(1, math.exp(log_acceptance_ratio))
+
+            q = rng.random()
+            # Accept/Reject
+            if (q < acceptance_probability):
+                current = proposed
+            else:
+                # Do nothing
+                pass
+
+            samples.append(copy.copy(current))
+            progress_bar.update(1)
+
+        progress_bar.close()
+        return samples
